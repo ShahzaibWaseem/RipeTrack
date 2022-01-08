b4f925d59cb947b31f234ea70b62b3ae1f04554d
Shahzaib Waseem <msw10@sfu.ca>
2022-01-08 15:11:18 -0800

    cleaned up the code (dimension flip and cleaned gt)

--- evaluate_model.py
+++ evaluate_model.py
@@ -21,10 +21,8 @@ def main():
 	for fusion in fusion_techniques:
 		save_point = torch.load(os.path.join(MODEL_PATH, fusion, checkpoint_file))
 		model_param = save_point['state_dict']
-
 		model = resblock(conv_bn_relu_res_block, block_num=10, input_channel=4, output_channel=51, fusion=fusion)
 		model.load_state_dict(model_param)
-
 		model = model.cuda()
 		model.eval()
 
@@ -34,7 +32,6 @@ def main():
 				logger.info("Fusion: %s\tDataset: %s\tIllumination: %s\n" % (fusion, test_dataset, illumination))
 
 				TEST_DATASET_DIR = os.path.join(TEST_ROOT_DATASET_DIR, "working_%s" % test_dataset, "%s_%s_204ch" % (test_dataset, illumination), "test")
-				
 				GT_PATH = os.path.join(TEST_DATASET_DIR, "mat")
 				IMG_PATH = os.path.join(TEST_DATASET_DIR, "cameraRGBN")
 				INF_PATH = os.path.join(TEST_DATASET_DIR, "inference")
@@ -48,41 +45,38 @@ def main():
 					rgb_img_path = img_name
 					nir_img_path = os.path.join(IMG_PATH, img_name.split("/")[-1].replace("RGB", "NIRc"))
 
-					rgb = imread(rgb_img_path)
-					
-					rgb_new = np.zeros(rgb.shape)
-					rgb_new[:,:,0] = rgb[:,:,2]
-					rgb_new[:,:,1] = rgb[:,:,1]
-					rgb_new[:,:,2] = rgb[:,:,0]
-					rgb = rgb_new
+					rgb = imread(rgb_img_path)/255
 
-					rgb = rgb/255
+					# flipping red and blue channels (shape used for training)
+					rgb[:,:, [0, 2]] = rgb[:,:, [2, 0]]
 
 					nir = imread(nir_img_path)/255
 
 					image = np.dstack((rgb, nir))
-					image = np.expand_dims(np.transpose(image,[2,1,0]), axis=0).copy()
+					# fixing the dimensions [Channel should be first in torch]
+					image = np.expand_dims(np.transpose(image, [2, 1, 0]), axis=0).copy()
 
-					img_res1 = reconstruction(image,model)
-					img_res2 = np.flip(reconstruction(np.flip(image, 2).copy(),model),1)
-					img_res3 = (img_res1+img_res2)/2
+					img_res1 = reconstruction(image, model)
+					img_res2 = np.flip(reconstruction(np.flip(image, 2).copy(), model), 1)
+					inf = (img_res1 + img_res2)/2
 
 					mat_name = "inf_" + mat_file_name + ".mat"
 					mat_dir = os.path.join(INF_PATH, fusion, mat_name)
-					save_matv73(mat_dir, var_name, img_res3)
+					save_matv73(mat_dir, var_name, inf)
 
 					gt_name = mat_file_name + ".mat"
 					gt_dir = os.path.join(GT_PATH, gt_name)
-					gt = load_mat(gt_dir, var_name)
-					mrae_error =  test_mrae(img_res3, gt[var_name][:,:,1:204:4])
-					rrmse_error = test_rmse(img_res3, gt[var_name][:,:,1:204:4])
-					sam_error = test_msam(img_res3, gt[var_name][:,:,1:204:4])
-					sid_error = test_sid(img_res3, gt[var_name][:,:,1:204:4])
-					psnr_error = test_psnr(img_res3, gt[var_name][:,:,1:204:4])
-					ssim_error = test_ssim(img_res3, gt[var_name][:,:,1:204:4])
+					gt = load_mat(gt_dir, var_name)[var_name][:,:, 1:204:4]
 
-					print(log_string %(mat_name, mrae_error, rrmse_error, sam_error, sid_error, psnr_error, ssim_error))
-					logger.info(log_string %(mat_name, mrae_error, rrmse_error, sam_error, sid_error, psnr_error, ssim_error))
+					mrae_error =  test_mrae(inf, gt)
+					rrmse_error = test_rmse(inf, gt)
+					sam_error = test_msam(inf, gt)
+					sid_error = test_sid(inf, gt)
+					psnr_error = test_psnr(inf, gt)
+					ssim_error = test_ssim(inf, gt)
+
+					print(log_string % (mat_name, mrae_error, rrmse_error, sam_error, sid_error, psnr_error, ssim_error))
+					logger.info(log_string % (mat_name, mrae_error, rrmse_error, sam_error, sid_error, psnr_error, ssim_error))
 
 if __name__ == "__main__":
 	init_directories()
