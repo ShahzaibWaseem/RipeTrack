9b2d2f54b12f36c519010e71e3ca5a9550edfd48
Shahzaib Waseem <msw10@sfu.ca>
2022-01-17 16:13:01 -0800

    removing unused imports, unused losses and docstrings to each loss/error function

--- loss.py
+++ loss.py
@@ -5,32 +5,15 @@ import numpy as np
 from skimage.metrics import structural_similarity
 
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-
-class reconstruct_loss(nn.Module):
-	"""the loss between the input and synthesized input"""
-	def __init__(self, cie_matrix, batchsize):
-		super(reconstruct_loss, self).__init__()
-		self.cie = Variable(torch.from_numpy(cie_matrix).float().cuda(), requires_grad=False)
-		self.batchsize = batchsize
-	def forward(self, network_input, network_output):
-		network_output = network_output.permute(3, 2, 0, 1)
-		network_output = network_output.contiguous().view(-1, 31)
-		reconsturct_input = torch.mm(network_output,self.cie)
-		reconsturct_input = reconsturct_input.view(50, 50, 64, 3)
-		reconsturct_input = reconsturct_input.permute(2,3,1,0)
-		reconstruction_loss = torch.mean(torch.abs(reconsturct_input - network_input))
-		return reconstruction_loss
 
 def mrae_loss(tensor_pred, tensor_gt):
-	""" Computes the MRAE value (Training Loss) """
+	""" Computes the Mean Relative Absolute Error Loss (PyTorch - Training Loss) """
 	error = torch.abs(tensor_pred-tensor_gt)/tensor_gt
 	rrmse = torch.mean(error.view(-1))
 	return rrmse
 
 def sam_loss(tensor_pred, tensor_gt):
-	""" Spectral Angle Mapper (Training Loss) """
+	""" Computes the Spectral Angle Mapper Loss (PyTorch - Training Loss) """
 	# inner product
 	dot = torch.sum(tensor_pred * tensor_gt, dim=1).view(-1)
 	# norm calculations
@@ -55,38 +38,44 @@ def sam_loss(tensor_pred, tensor_gt):
 		)
 	return loss
 
+def mse(img_pred, img_gt):
+	""" Calculate the mean square error (NumPy - used in test_psnr())"""
+	error = (img_pred - img_gt)
+	mse = np.mean((np.power(error, 2)))
+	return mse
+
+def spectral_angle(a, b):
+	""" Spectral angle between two arrays (NumPy - used in test_msam()) """
+	va = a / np.sqrt(a.dot(a))
+	vb = b / np.sqrt(b.dot(b))
+	return np.arccos(va.dot(vb))
+
+def spectral_divergence(a, b):
+	""" Spectral Divergence between two arrays (NumPy - used in test_sid()) """
+	p = (a / np.sum(a)) + np.spacing(1)
+	q = (b / np.sum(b)) + np.spacing(1)
+	return np.sum(p * np.log(p / q) + q * np.log(q / p))
+
 def test_mrae(img_pred, img_gt):
-	"""Calculate the relative MRAE"""
+	""" Calculate the relative Mean Relative Absolute Error (NumPy - Test Error) """
 	error = img_pred - img_gt
 	error_relative = error/img_gt
 	rrmse = np.mean(np.abs(error_relative))
 	return rrmse
 
 def test_rrmse(img_pred, img_gt):
+	""" Calculate the relative Root Mean Square Error (NumPy - Test Error) """
 	error = img_pred - img_gt
 	error_relative = error/img_gt
 	rrmse = np.sqrt(np.mean((np.power(error_relative, 2))))
 	return rrmse
 
-def spectral_angle(a, b):
-	""" Spectral angle """
-	va = a / np.sqrt(a.dot(a))
-	vb = b / np.sqrt(b.dot(b))
-	# print(va.shape, vb.shape)
-	return np.arccos(va.dot(vb))
-
 def test_msam(img_pred, img_gt):
-	""" mean spectral angle mapper """
+	""" Calculate the mean spectral angle mapper (NumPy - Test Error) """
 	img_pred_flat = img_pred.reshape(-1, img_pred.shape[2])
 	img_gt_flat = img_gt.reshape(-1, img_gt.shape[2])
 	assert len(img_pred_flat) == len(img_gt_flat)
 	return np.mean([spectral_angle(img_pred_flat[i]/4095, img_gt_flat[i]/4095) for i in range(len(img_pred_flat))])
-
-def spectral_divergence(a, b):
-	""" Spectral Divergence """
-	p = (a / np.sum(a)) + np.spacing(1)
-	q = (b / np.sum(b)) + np.spacing(1)
-	return np.sum(p * np.log(p / q) + q * np.log(q / p))
 
 def test_sid(img_pred, img_gt):
 	""" mean spectral information divergence """
@@ -95,16 +84,10 @@ def test_sid(img_pred, img_gt):
 	assert len(img_pred_flat) == len(img_gt_flat)
 	return np.mean([spectral_divergence(img_pred_flat[i]/4095, img_gt_flat[i]/4095) for i in range(len(img_pred_flat))])
 
-def mse(img_pred, img_gt):
-	error = (img_pred - img_gt)
-	mse = np.mean((np.power(error, 2)))
-	return mse
-
 def test_psnr(img_pred, img_gt):
+	""" Calculate the peak signal to noise ratio (NumPy - Test Error) """
 	return 10 * np.log10(4095**2 / mse(img_pred, img_gt))
 
 def test_ssim(img_pred, img_gt, max_p=4095):
-	"""
-	Structural Simularity Index
-	"""
+	""" Calculate the structural simularity index measure (NumPy - Test Error) """
 	return structural_similarity(img_gt, img_pred, data_range=max_p, channel_axis=True)
