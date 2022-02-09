bdd04a073d8e5c21c20820ee98a92d9792d3df09
Shahzaib Waseem <msw10@sfu.ca>
2022-02-08 17:09:33 -0800

    stopped cfl_led chicken folder creation and correct order of variables

--- config.py
+++ config.py
@@ -32,12 +32,13 @@ TEST_ROOT_DATASET_DIR = os.path.join(os.path.dirname(TRAIN_DATASET_DIR), "workin
 TEST_DATASETS = ["steak", "chicken"]
 
 checkpoint_fileprestring = "%s_%s" % (MODEL_NAME, DATASET_NAME)
-checkpoint_file = "MS_%s_23.pkl" % checkpoint_fileprestring
+checkpoint_file = "MS_%s_49.pkl" % checkpoint_fileprestring
+# checkpoint_file = "HS_model_%d.pkl" % end_epoch
+
 mobile_model_file = "model_%s.pth" % DATASET_NAME
 onnx_file_name = "model.onnx"
 tf_model_dir = os.path.join("tfmodel")
 tflite_filename = "model.tflite"
-# checkpoint_file = "HS_model_%d.pkl" % end_epoch
 
 plt_dict = {"mathtext.default": "regular",
 			"axes.linewidth": 2}
@@ -61,6 +62,9 @@ def init_directories():
 	for test_dataset in TEST_DATASETS:
 		for illumination in ILLUMINATIONS:
 			for directory in ["inference", "images"]:
+				# ignore making directories for chicken cfl-led dataset
+				if illumination == "cfl_led" and test_dataset == "chicken":
+					continue
 				test_dataset_path = os.path.join(TEST_ROOT_DATASET_DIR, "working_%s" % test_dataset, "%s_%s_204ch" % (test_dataset, illumination), "test", directory, MODEL_NAME)
 				if not os.path.exists(test_dataset_path):
 					os.makedirs(test_dataset_path)
