4a8e0be88a26415bcf32062fe6b36aca99739ac5
Shahzaib Waseem <msw10@sfu.ca>
2022-06-02 13:04:38 -0700

    added a function to recieve mean and standard deviation of the dataset.

--- dataset.py
+++ dataset.py
@@ -53,6 +53,23 @@ def read_image(rgb_filename, nir_filename):
 
 	return image
 
+def get_normalization_parameters(dataloader):
+	""" Give Dataloader and recieve the mean and std of the dataset.
+		Note: Make sure that the dataloader is Tensordataset and its not already normalized. """
+	channels_sum, channels_squared_sum, num_batches = 0, 0, 0
+	for data, _ in dataloader:
+		# Mean over batch, height and width, but not over the channels
+		channels_sum += torch.mean(data, dim=[0, 2, 3])
+		channels_squared_sum += torch.mean(data**2, dim=[0, 2, 3])
+		num_batches += 1
+
+	mean = channels_sum / num_batches
+
+	# std = sqrt(E[X^2] - (E[X])^2)
+	std = (channels_squared_sum / num_batches - mean ** 2) ** 0.5
+
+	return mean, std
+
 class DatasetFromDirectory(Dataset):
 	# Expects the directory structure to be:
 	# root/
