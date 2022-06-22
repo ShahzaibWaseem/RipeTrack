389b7341ae457a4062fd0ffb0448d10e7235621c
Shahzaib Waseem <msw10@sfu.ca>
2022-06-21 17:50:35 -0700

    standardizing the code throughout the files

--- loss.py
+++ loss.py
@@ -4,9 +4,9 @@ from __future__ import division
 import numpy as np
 from skimage.metrics import structural_similarity
 
-from config import OUTPUT_BANDS
-
 import torch
+
+from config import NORMALIZATION_FACTOR
 
 def mrae_loss(tensor_pred, tensor_gt):
 	""" Computes the Mean Relative Absolute Error Loss (PyTorch - Training Loss) """
@@ -28,16 +28,14 @@ def sam_loss(tensor_pred, tensor_gt):
 	norm_product = (norm_original.mul(norm_reconstructed)).pow(-1)
 	argument = dot.mul(norm_product)
 	# for avoiding arccos(1)
-	acos = torch.acos(torch.clamp(argument, -1 + 1e-7, 1 - 1e-7))
+	acos = torch.acos(torch.clamp(argument, min=-1+1e-7, max=1-1e-7))
 	loss = torch.mean(acos)
 
 	if torch.isnan(loss):
-		raise ValueError(
-			f"Loss is NaN value. Consecutive values - dot: {dot}, \
-			norm original: {norm_original}, norm reconstructed: {norm_reconstructed}, \
-			norm product: {norm_product}, argument: {argument}, acos: {acos}, \
-			loss: {loss}, input: {tensor_pred}, output: {target}"
-		)
+		raise ValueError(f"Loss is NaN value. Consecutive values - dot: {dot},\
+			norm original: {norm_original}, norm reconstructed: {norm_reconstructed},\
+			norm product: {norm_product}, argument: {argument}, acos: {acos},\
+			loss: {loss}, input: {tensor_pred}, output: {target}")
 	return loss
 
 def sid_loss(tensor_pred, tensor_gt):
@@ -95,19 +93,19 @@ def test_msam(img_pred, img_gt):
 	img_pred_flat = img_pred.reshape(-1, img_pred.shape[2])
 	img_gt_flat = img_gt.reshape(-1, img_gt.shape[2])
 	assert len(img_pred_flat) == len(img_gt_flat)
-	return np.mean([spectral_angle(img_pred_flat[i]/4095, img_gt_flat[i]/4095) for i in range(len(img_pred_flat))])
+	return np.mean([spectral_angle(img_pred_flat[i]/NORMALIZATION_FACTOR, img_gt_flat[i]/NORMALIZATION_FACTOR) for i in range(len(img_pred_flat))])
 
 def test_sid(img_pred, img_gt):
 	""" mean spectral information divergence """
 	img_pred_flat = img_pred.reshape(-1, img_pred.shape[2])
 	img_gt_flat = img_gt.reshape(-1, img_gt.shape[2])
 	assert len(img_pred_flat) == len(img_gt_flat)
-	return np.mean([spectral_divergence(img_pred_flat[i]/4095, img_gt_flat[i]/4095) for i in range(len(img_pred_flat))])
+	return np.mean([spectral_divergence(img_pred_flat[i]/NORMALIZATION_FACTOR, img_gt_flat[i]/NORMALIZATION_FACTOR) for i in range(len(img_pred_flat))])
 
 def test_psnr(img_pred, img_gt):
 	""" Calculate the peak signal to noise ratio (NumPy - Test Error) """
-	return 10 * np.log10(4095**2 / mse(img_pred, img_gt))
+	return 10 * np.log10(NORMALIZATION_FACTOR**2 / mse(img_pred, img_gt))
 
-def test_ssim(img_pred, img_gt, max_p=4095):
+def test_ssim(img_pred, img_gt, max_p=NORMALIZATION_FACTOR):
 	""" Calculate the structural simularity index measure (NumPy - Test Error) """
 	return structural_similarity(img_gt, img_pred, data_range=max_p, channel_axis=True)
