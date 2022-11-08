ca2ff78fe117437ac605299253582799bbd3e70a
Shahzaib Waseem <msw10@sfu.ca>
2022-11-07 16:00:21 -0800

    dynamic element in the model title string, predefined transforms update, added a function for making directories

--- config.py
+++ config.py
@@ -31,6 +31,7 @@ EXTRACT_DATASETS = ["ambrosia-nonorganic", "ambrosia-organic", "gala-nonorganic"
 BAND_SPACING = 1						# used only if reading data from directories dataset.DatasetFromDirectory
 NUMBER_OF_BANDS = 204//BAND_SPACING		# holds the number of bands considered (used in model creation)
 
+IMAGE_SIZE = 512
 PATCH_SIZE = 512						# patching of the hypercubes and images
 NORMALIZATION_FACTOR = 4096				# max value of the captured hypercube (dependent on the camera - Specim IQ)
 RGBN_BANDS = [18, 47, 80, 183]			# correspond to B 454, G 541, R 640, N 949 bands
@@ -54,10 +55,10 @@ elif "h" in ILLUMINATIONS:
 elif "cfl_led" in ILLUMINATIONS:
 	illumination_string = "CFL-LED"
 
-model_run_title = "Model: %s\tDataset: %s\tIllumination: %s\tLosses: MRAE + SAM + SID + Weighted\tFull Image or Patches: patches\n" \
-	% (MODEL_NAME, DATASET_NAME, illumination_string)
-classicication_run_title = "Model: %s\tDataset: %s\tIllumination: %s\tNumber of Classes: %d\tFull Image or Patches: patches\n" \
-	% (CLASSIFIER_MODEL_NAME, DATASET_NAME, illumination_string, len(TEST_DATASETS))
+model_run_title = "Model: %s\tDataset: %s\tIllumination: %s\tLosses: MRAE + SAM + SID + Weighted\tFull Image or Patches: %s\n" \
+	% (MODEL_NAME, DATASET_NAME, illumination_string, "Full Image" if PATCH_SIZE == IMAGE_SIZE else "Patches")
+classicication_run_title = "Model: %s\tDataset: %s\tIllumination: %s\tNumber of Classes: %d\tFull Image or Patches: %s\n" \
+	% (CLASSIFIER_MODEL_NAME, DATASET_NAME, illumination_string, len(TEST_DATASETS), "Full Image" if PATCH_SIZE == IMAGE_SIZE else "Patches")
 
 ### to create the checkpoint of the model ###
 checkpoint_fileprestring = "%s_%s" % (MODEL_NAME, DATASET_NAME)
@@ -77,19 +78,23 @@ title_font_dict = {"fontname": "serif", "size": 25}
 ### Predefined Transforms (Just to speed the process up)
 from torchvision import transforms
 
-predef_input_transform, predef_label_transform = None, transforms.Compose([transforms.Normalize(
-	mean=[0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004,
-	0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004,
-	0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004,
-	0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004,
-	0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004,
-	0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004, 0.0004],
-	std=[0.0002, 0.0002, 0.0002, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003,
-	0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003,
-	0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003,
-	0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003,
-	0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003,
-	0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003, 0.0003])])
+predef_input_transform = transforms.Compose([transforms.Normalize(
+	mean=[0.0752, 0.1171, 0.2212, 0.1676],
+	std=[0.0726, 0.1514, 0.2097, 0.1509])])
+
+predef_label_transform = transforms.Compose([transforms.Normalize(
+	mean=[0.0049, 0.0068, 0.0167, 0.0215, 0.0218, 0.0225, 0.0229, 0.0231, 0.0232,
+	0.0232, 0.0231, 0.0232, 0.0233, 0.0234, 0.0237, 0.0237, 0.0237, 0.0240,
+	0.0241, 0.0242, 0.0244, 0.0245, 0.0246, 0.0246, 0.0245, 0.0244, 0.0241,
+	0.0240, 0.0239, 0.0239, 0.0238, 0.0237, 0.0237, 0.0237, 0.0235, 0.0234,
+	0.0233, 0.0232, 0.0231, 0.0230, 0.0228, 0.0225, 0.0222, 0.0217, 0.0213,
+	0.0207, 0.0201, 0.0199, 0.0200, 0.0201, 0.0204, 0.0207, 0.0212, 0.0215],
+	std=[0.0045, 0.0088, 0.0160, 0.0189, 0.0221, 0.0229, 0.0233, 0.0235, 0.0235,
+	0.0233, 0.0231, 0.0231, 0.0231, 0.0233, 0.0236, 0.0235, 0.0235, 0.0237,
+	0.0238, 0.0240, 0.0242, 0.0243, 0.0244, 0.0245, 0.0244, 0.0241, 0.0238,
+	0.0236, 0.0235, 0.0234, 0.0233, 0.0232, 0.0231, 0.0230, 0.0228, 0.0226,
+	0.0224, 0.0222, 0.0220, 0.0217, 0.0213, 0.0208, 0.0202, 0.0193, 0.0185,
+	0.0174, 0.0165, 0.0160, 0.0158, 0.0156, 0.0157, 0.0157, 0.0159, 0.0160])])
 
 ### Bands for text in the visualizations ###
 VIEW_BANDS = [11, 21, 36, 44]
@@ -117,11 +122,17 @@ def sampler():
 
 sampler()
 
+def create_directory(directory):
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+
 def init_directories():
 	""" Creates directories if they don't exist which are used to save model, logs, generated hypercubes and visualizations """
 	for directory in [MODEL_PATH, LOGS_PATH]:
-		if not os.path.exists(directory):
-			os.makedirs(directory)
+		if directory == MODEL_PATH:
+			create_directory(os.path.join(directory, "reconstruction"))
+			create_directory(os.path.join(directory, "classification"))
+		create_directory(directory)
 
 	for test_dataset in TEST_DATASETS:
 		for illumination in ILLUMINATIONS:
@@ -131,5 +142,4 @@ def init_directories():
 					continue
 				test_dataset_path = os.path.join(TEST_ROOT_DATASET_DIR, "working_%s" % DATASET_NAME, "working_%s" % test_dataset, "%s_%s_204ch" \
 					% (test_dataset, illumination), "test", directory, MODEL_NAME)
-				if not os.path.exists(test_dataset_path):
-					os.makedirs(test_dataset_path)
+				create_directory(test_dataset_path)
