1d8a13782d635b427885d5a6f1acfdacf1b53d33
Shahzaib Waseem <msw10@sfu.ca>
2022-11-07 22:51:51 -0800

    moved transform related functions to `utils.py`

--- train.py
+++ train.py
@@ -7,134 +7,21 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 from torchsummary import summary
-from torchvision import transforms
 from torch.autograd import Variable
-from torch.utils.data import DataLoader, ConcatDataset, random_split
 
 from models.model import Network
-from models.resblock import resblock, ResNeXtBottleneck
+from models.resblock import ResNeXtBottleneck
 from loss import mrae_loss, sam_loss, sid_loss, weighted_loss
-from dataset import DatasetFromDirectory, DatasetFromHdf5, get_normalization_parameters
 
-from utils import AverageMeter, initialize_logger, save_checkpoint, get_best_checkpoint, record_loss, makeMobileModel, make_h5_dataset, modeltoONNX, ONNXtotf, tf_to_tflite
-from config import TEST_ROOT_DATASET_DIR, TRAIN_DATASET_DIR, TRAIN_DATASET_FILES, VALID_DATASET_FILES, LOGS_PATH, MODEL_NAME, DATASET_NAME, NUMBER_OF_BANDS, BANDS, PATCH_SIZE, init_directories, checkpoint_file, batch_size, end_epoch, init_lr, model_run_title, run_pretrained
+from utils import AverageMeter, get_dataloaders, get_required_transforms, initialize_logger, save_checkpoint, get_best_checkpoint, record_loss, makeMobileModel, make_h5_dataset, modeltoONNX, ONNXtotf, tf_to_tflite, poly_lr_scheduler
+from config import LOGS_PATH, BANDS, init_directories, batch_size, end_epoch, init_lr, model_run_title, run_pretrained
 
 torch.autograd.set_detect_anomaly(True)
 
-def get_required_transforms(task="reconstruction"):
-	""" Returns the two transforms for the RGB-NIR image input and Hypercube label.
-		Note: The `dataset` recieved is already Tensor data.
-		This function gets the dataset specified in the `config.py` file. """
-	# Dataset
-	image_mean, image_std, hypercube_mean, hypercube_std = 0, 0, 0, 0
-
-	dataset = DatasetFromDirectory(root=TEST_ROOT_DATASET_DIR,
-								   dataset_name=DATASET_NAME,
-								   task=task,
-								   patch_size=PATCH_SIZE,
-								   lazy_read=False,
-								   shuffle=False,
-								   rgbn_from_cube=False,
-								   use_all_bands=False if task == "reconstruction" else True,
-								   product_pairing=False,
-								   train_with_patches=True,
-								   positive_only=False,
-								   verbose=False,
-								   augment_factor=0,
-								   transform=(None, None))
-
-	dataloader = DataLoader(dataset=dataset,
-							num_workers=1,
-							batch_size=batch_size,
-							shuffle=False,
-							pin_memory=True)
-
-	(image_mean, image_std), (hypercube_mean, hypercube_std) = get_normalization_parameters(dataloader)
-
-	print(75*"-" + "\nDataset Normalization\n" + 75*"-")
-
-	if task == "reconstruction":
-		print("RGB-NIR Images Size:\t\t\t\t\t%d" % (image_mean.size(dim=0)))
-		print("The Mean of the dataset is in the range:\t\t%f - %f"
-			% (torch.min(image_mean).item(), torch.max(image_mean).item()))
-		print("The Standard Deviation of the dataset is in the range:\t%f - %f\n"
-			% (torch.min(image_std).item(), torch.max(image_std).item()))
-
-	print("Hypercubes Size:\t\t\t\t\t%d" % (hypercube_std.size(dim=0)))
-	print("The Mean of the dataset is in the range:\t\t%f - %f"
-		  % (torch.min(hypercube_mean).item(), torch.max(hypercube_mean).item()))
-	print("The Standard Deviation of the dataset is in the range:\t%f - %f"
-		  % (torch.min(hypercube_std).item(), torch.max(hypercube_std).item()))
-	print(75*"-")
-	del dataset, dataloader
-
-	hypercube_transform_list = []
-
-	if task == "classification":
-		hypercube_transform_list = [# transforms.Resize((PATCH_SIZE, PATCH_SIZE)),
-									# transforms.RandomResizedCrop(size=(PATCH_SIZE, PATCH_SIZE)),
-									# transforms.CenterCrop(size=(PATCH_SIZE, PATCH_SIZE)),
-									transforms.RandomRotation(20, interpolation=transforms.InterpolationMode.BILINEAR),
-									transforms.RandomHorizontalFlip()]
-
-	# Data is already tensor, so just normalize it
-	hypercube_transform_list.append(transforms.Normalize(mean=hypercube_mean, std=hypercube_std))
-	input_transform = transforms.Compose([transforms.Normalize(mean=image_mean, std=image_std)]) if task == "reconstruction" else None
-	hypercube_transform = transforms.Compose([transforms.Normalize(mean=hypercube_mean, std=hypercube_std)])
-
-	del hypercube_transform_list
-	return input_transform, hypercube_transform
-
 def main():
-	torch.backends.cudnn.benchmark = True
-
+	trainset_size=0.8
 	input_transform, label_transform = get_required_transforms()
-	dataset = DatasetFromDirectory(root=TEST_ROOT_DATASET_DIR,
-								   dataset_name=DATASET_NAME,
-								   task="reconstruction",
-								   patch_size=PATCH_SIZE,
-								   lazy_read=False,
-								   shuffle=True,
-								   rgbn_from_cube=False,
-								   use_all_bands=False,
-								   product_pairing=False,
-								   train_with_patches=True,
-								   positive_only=True,
-								   verbose=True,
-								   transform=(input_transform, label_transform))
-
-	trainset_size = 0.8
-	print("Dataset size:\t\t\t{}".format(len(dataset)))
-
-	train_data, valid_data = random_split(dataset, [int(trainset_size*len(dataset)), len(dataset) - int(len(dataset)*trainset_size)])
-	print("Length of Training Set ({}%):\t{}".format(round(trainset_size * 100), len(train_data)))
-	print("Length of Validation Set ({}%):\t{}".format(round((1-trainset_size) * 100), len(valid_data)))
-
-	# train_data, valid_data = [], []
-	# for datasetFile in TRAIN_DATASET_FILES:
-	# 	h5_filepath = os.path.join(TRAIN_DATASET_DIR, datasetFile)
-	# 	dataset = DatasetFromHdf5(h5_filepath)
-	# 	train_data.append(dataset)
-	# 	print("Length of Training Set (%s):" % datasetFile, len(dataset))
-
-	# for datasetFile in VALID_DATASET_FILES:
-	# 	h5_filepath = os.path.join(TRAIN_DATASET_DIR, datasetFile)
-	# 	dataset = DatasetFromHdf5(h5_filepath)
-	# 	valid_data.append(dataset)
-	# 	print("Length of Validation Set (%s):" % datasetFile, len(dataset))
-
-	# Data Loader (Input Pipeline)
-	train_data_loader = DataLoader(dataset=train_data,
-								   num_workers=1,
-								   batch_size=batch_size,
-								   shuffle=True,
-								   pin_memory=True)
-
-	val_data_loader = DataLoader(dataset=valid_data,
-								 num_workers=1,
-								 batch_size=2,
-								 shuffle=False,
-								 pin_memory=True)
+	train_data_loader, val_data_loader, whole_dataset_loader = get_dataloaders(input_transform, label_transform, task="reconstruction", trainset_size=trainset_size)
 
 	# Parameters, Loss and Optimizer
 	start_epoch = 0
@@ -185,7 +72,7 @@ def main():
 	for epoch in range(start_epoch+1, end_epoch):
 		start_time = time.time()
 
-		train_loss, train_losses_ind, iteration, lr = train(train_data_loader, model, criterions, optimizer, iteration, init_lr, int(trainset_size*len(dataset))*end_epoch/batch_size)
+		train_loss, train_losses_ind, iteration, lr = train(train_data_loader, model, criterions, optimizer, iteration, init_lr, int(trainset_size*len(whole_dataset_loader))*end_epoch/batch_size)
 		val_loss, val_losses_ind = validate(val_data_loader, model, criterions)
 
 		train_loss_mrae, train_loss_sam, train_loss_sid = train_losses_ind
@@ -281,27 +168,9 @@ def validate(val_data_loader, model, criterions):
 
 	return losses.avg, (losses_mrae.avg, losses_sam.avg, losses_sid.avg)
 
-def poly_lr_scheduler(optimizer, init_lr, iteraion, lr_decay_iter=1, max_iter=100, power=0.9):
-	"""
-	Polynomial decay of learning rate
-		init_lr:		base learning rate
-		iter:			current iteration
-		lr_decay_iter:	how frequently decay occurs, default is 1
-		max_iter:		number of maximum iterations
-		power:			polymomial power
-	"""
-	if iteraion % lr_decay_iter or iteraion > max_iter:
-		return optimizer
-
-	lr = init_lr * (1 - iteraion/max_iter) ** power
-
-	for param_group in optimizer.param_groups:
-		param_group["lr"] = lr
-
-	return lr
-
 if __name__ == "__main__":
-	# init_directories()
+	torch.backends.cudnn.benchmark = True
+	init_directories()
 	# makeMobileModel()
 	# make_h5_dataset(TRAIN_DATASET_DIR=os.path.join(TRAIN_DATASET_DIR, "train"), h5_filename="train_apple_halogen_4to51bands_whole.h5")
 	# make_h5_dataset(TRAIN_DATASET_DIR=os.path.join(TRAIN_DATASET_DIR, "valid"), h5_filename="valid_apple_halogen_4to51bands_whole.h5")
