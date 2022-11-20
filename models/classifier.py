178c354d0aa6fc5b01a3b23f5693e91370eb4369
Shahzaib Waseem <msw10@sfu.ca>
2022-11-20 12:35:56 -0800

    adding a new multihead classifier to capture different features in different bands

--- models/classifier.py
+++ models/classifier.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from efficientnet_pytorch import EfficientNet
 
-from config import BANDS, TEST_DATASETS, CLASSIFIER_MODEL_NAME
+from config import BANDS, TEST_DATASETS, CLASSIFIER_MODEL_NAME, PATCH_SIZE
 
 class TorchClassifier(nn.Module):
 	def __init__(self, fine_tune=True, in_channels=len(BANDS), num_classes=len(TEST_DATASETS)):
@@ -47,4 +47,49 @@ class SeparateClassifiers(nn.Module):
 		nir_x = self.nir_module(nir_x)
 		x = torch.cat((rgb_x, nir_x), dim=1)
 		x = self.relu(self.fc(x))
+		return x
+
+class FeatureExtractionBlock(nn.Module):
+	def __init__(self, input_channels):
+		super().__init__()
+		intermediate_channels = 16
+		self.conv1 = nn.Conv2d(input_channels, intermediate_channels, kernel_size=3, stride=1, padding=1)
+		self.bn1 = nn.BatchNorm2d(intermediate_channels)
+		self.relu = nn.ReLU(inplace=True)
+		self.conv2 = nn.Conv2d(intermediate_channels, intermediate_channels, kernel_size=3, stride=1, padding=1)
+		self.bn2 = nn.BatchNorm2d(intermediate_channels)
+
+	def forward(self, x):
+		# residual = x
+		x = self.bn1(self.conv1(x))
+		x = self.relu(x)
+		x = self.bn2(self.conv2(x))
+		# x = torch.mul(x, 0.1)
+		# x = torch.add(x, residual)
+		return x
+
+class MultiHeadClassification(nn.Module):
+	def __init__(self, input_channels=3, groups=3, num_classes=2):
+		super().__init__()
+		bands_head1, bands_head2 = BANDS[:16], BANDS[-17:]
+		self.feature_ex_block1 = nn.Sequential(FeatureExtractionBlock(len(bands_head1)), nn.MaxPool2d(2, 2), FeatureExtractionBlock(16), FeatureExtractionBlock(16), FeatureExtractionBlock(16))
+		self.feature_ex_block2 = nn.Sequential(FeatureExtractionBlock(len(bands_head2)), nn.MaxPool2d(2, 2), FeatureExtractionBlock(16), FeatureExtractionBlock(16), FeatureExtractionBlock(16))
+		# self.fusion_layer = nn.Conv2d(16+16, 32, kernel_size=3, stride=1, padding=1)
+		# self.bn = nn.BatchNorm2d(32)
+		self.relu = nn.ReLU(inplace=True)
+		self.bottleneck = nn.Linear(in_features=32*PATCH_SIZE*PATCH_SIZE//4, out_features=256, bias=True)
+		self.dropout = nn.Dropout(p=0.5)
+		self.fc = nn.Linear(in_features=256, out_features=num_classes)
+
+	def forward(self, x):
+		x1 = x[:, :16, :, :]
+		x2 = x[:, -17:, :, :]
+		x1 = self.feature_ex_block1(x1)
+		x2 = self.feature_ex_block2(x2)
+		x = torch.cat((x1, x2), dim=1)
+		# x = self.relu(self.bn(self.fusion_layer(x)))
+		x = x.view(x.size(0), -1)
+		x = self.relu(self.bottleneck(x))
+		x = self.dropout(x)
+		x = self.fc(x)
 		return x
