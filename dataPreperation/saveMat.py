75ebf9049fe739b5b9afaddd2251efa0a2be9506
Shahzaib Waseem <msw10@sfu.ca>
2023-08-30 15:24:01 -0700

    Updated to save aligned images, and save secondary images from HS cam

--- dataPreperation/saveMat.py
+++ dataPreperation/saveMat.py
@@ -3,18 +3,20 @@ import sys
 sys.path.append("..")
 
 import imageio
+from PIL import Image
+
 import numpy as np
 import pandas as pd
 
 from spectral import envi
 from scipy.io import savemat
 
-from config import CAMERA_OUTPUT_ROOT_PATH, TEST_ROOT_DATASET_DIR, APPLICATION_NAME, RGBN_BANDS, SHELF_LIFE_GROUND_TRUTH_FILENAME, GT_RGBN_DIR_NAME, var_name, create_directory
+from config import CAMERA_OUTPUT_ROOT_PATH, TEST_ROOT_DATASET_DIR, APPLICATION_NAME, RGBN_BANDS, SHELF_LIFE_GROUND_TRUTH_FILENAME, GT_RGBN_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME, var_name, create_directory
 
 import matplotlib.pyplot as plt
 
-def plotImages(rgb_image, nir_image, hypercube):
-	fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
+def plotImages(rgb_image, nir_image, hypercube, rgb_secondary_image):
+	fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(35, 30))
 	ax1.imshow(rgb_image)
 	ax1.title.set_text("RGB Image")
 	ax2.imshow(nir_image)
@@ -22,8 +24,10 @@ def plotImages(rgb_image, nir_image, hypercube):
 	rgb_from_cube = hypercube[var_name][:,:, RGBN_BANDS[:3]]
 	rgb_from_cube = bandToNpNormalize(rgb_from_cube)
 	rgb_from_cube[:, :, [0, 2]] = rgb_from_cube[:, :, [2, 0]]
-	ax3.imshow(rgb_from_cube)
-	ax3.title.set_text("Hypercube")
+	ax3.imshow(rgb_secondary_image)
+	ax3.title.set_text("RGB Secondary Image")
+	ax4.imshow(rgb_from_cube)
+	ax4.title.set_text("Hypercube")
 	plt.show()
 
 def bandToNpNormalize(image):
@@ -42,8 +46,10 @@ def main():
 
 		output_hypercube_directory = os.path.join("..", TEST_ROOT_DATASET_DIR, APPLICATION_NAME, "%s_204ch" % dataset_name)
 		output_rgbn_directory = os.path.join(output_hypercube_directory, GT_RGBN_DIR_NAME)
+		output_secondary_rgbn_directory = os.path.join(output_hypercube_directory, GT_SECONDARY_RGB_CAM_DIR_NAME)
 		create_directory(output_hypercube_directory)
 		create_directory(output_rgbn_directory)
+		create_directory(output_secondary_rgbn_directory)
 
 		input_catalog_directory = os.path.join(root_directory, "working_%s" % APPLICATION_NAME)
 
@@ -59,7 +65,8 @@ def main():
 
 			rgb_secondary_filepath = os.path.join(input_catalog_directory, "%d" % hs_filenumber, "results", "RGBBACKGROUND_%s.png" % hs_filenumber)
 			rgb_secondary_image = imageio.imread(rgb_secondary_filepath)[:, :, :3]
-			# CROP TO 512x512
+			rgb_secondary_image = Image.fromarray(rgb_secondary_image)
+			rgb_secondary_image = np.asarray(rgb_secondary_image.resize((512, 512)))
 
 			nir_image = np.expand_dims(np.rot90(hypercube.read_band(RGBN_BANDS[-1]), k=-1), axis=-1)
 			nir_image = bandToNpNormalize(nir_image)
@@ -67,16 +74,19 @@ def main():
 			hypercube = hypercube.load()*256
 			hypercube = {var_name: np.rot90(hypercube, k=-1)}
 
-			plotImages(rgb_image, nir_image, hypercube)
+			# plotImages(rgb_image, nir_image, hypercube, rgb_secondary_image)
 
 			print("[%30s] Hypercube Shape: %s [Min: %d, Max: %d],\tRGB Image Shape: %s [Min: %d, Max: %d],\tNIR Image Shape: %s [Min: %d, Max: %d],\tRGB Secondary Image Shape: %s [Min: %d, Max: %d]"
 	 			% (os.path.join(output_hypercube_directory.split("/")[-1], "%s.mat" % hs_filenumber), hypercube[var_name].shape, hypercube[var_name].min(), hypercube[var_name].max(),
 				   rgb_image.shape, np.min(rgb_image), np.max(rgb_image), nir_image.shape, np.min(nir_image), np.max(nir_image), rgb_secondary_image.shape, np.min(rgb_secondary_image), np.max(rgb_secondary_image)))
 
-			# savemat(os.path.join(output_hypercube_directory, "%s.mat" % hs_filenumber), hypercube)
+			savemat(os.path.join(output_hypercube_directory, "%s.mat" % hs_filenumber), hypercube)
 
-			# imageio.imwrite(os.path.join(output_rgbn_directory, "%s_RGB.png" % hs_filenumber), rgb_image)
-			# imageio.imwrite(os.path.join(output_rgbn_directory, "%s_NIR.png" % hs_filenumber), nir_image)
+			imageio.imwrite(os.path.join(output_rgbn_directory, "%s_RGB.png" % hs_filenumber), rgb_image)
+			imageio.imwrite(os.path.join(output_rgbn_directory, "%s_NIR.png" % hs_filenumber), nir_image)
+
+			imageio.imwrite(os.path.join(output_secondary_rgbn_directory, "%s_RGB.png" % hs_filenumber), rgb_secondary_image)
+			imageio.imwrite(os.path.join(output_secondary_rgbn_directory, "%s_NIR.png" % hs_filenumber), nir_image)
 
 if __name__ == "__main__":
 	main()
--- dataPreperation/sortMobilePhotos.py
+++ dataPreperation/sortMobilePhotos.py
@@ -2,11 +2,13 @@ import os
 import sys
 sys.path.append("..")
 
-import pandas as pd
+import imageio
+from PIL import Image
 from glob import glob
-from shutil import copy2
 
-from config import TEST_ROOT_DATASET_DIR, APPLICATION_NAME, SHELF_LIFE_GROUND_TRUTH_FILENAME, MOBILE_DATASET_DIR_NAME, create_directory
+import pandas as pd
+
+from config import TEST_ROOT_DATASET_DIR, APPLICATION_NAME, SHELF_LIFE_GROUND_TRUTH_FILENAME, MOBILE_DATASET_DIR_NAME, VISUALIZATION_DIR_NAME, create_directory
 
 import matplotlib.pyplot as plt
 
@@ -18,12 +20,32 @@ def plotImages(image):
 	ax.title.set_text("RGB Image")
 	plt.show()
 
-def fixedAlign(image1, image2):
-	aligningFactorX = 37
-	aligningFactorY = 87
+def fixedAlign(image):
+	aligningFactorX = 35
+	aligningFactorY = 82
+	mobileImageXShape = 480
+	mobileImageYShape = 640
+
+	image = image[aligningFactorY:aligningFactorY+mobileImageYShape, aligningFactorX:aligningFactorX+mobileImageXShape, :]
+	return image
+
+def viewImages(rgb_image, nir_image, rgb_image_aligned, plot_title, dataset_name, plot_filename):
+	rgb_image_for = Image.fromarray(rgb_image_aligned)
+	nir_image_for = Image.fromarray(nir_image)
+	fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(35, 30))
+	fig.suptitle(plot_title)
+	ax1.imshow(rgb_image)
+	ax2.imshow(nir_image)
+	ax3.imshow(rgb_image_aligned)
+	merged_image = Image.blend(rgb_image_for, nir_image_for, 0.5)
+	ax4.imshow(merged_image)
+	create_directory(os.path.join("..", TEST_ROOT_DATASET_DIR, APPLICATION_NAME, "%s_204ch" % dataset_name, VISUALIZATION_DIR_NAME))
+	plt.savefig(os.path.join("..", TEST_ROOT_DATASET_DIR, APPLICATION_NAME, "%s_204ch" % dataset_name, VISUALIZATION_DIR_NAME, "%s.png" % plot_filename))
+	plt.close()
 
 def main():
 	ground_truth_df = pd.read_csv(os.path.join(SHELF_LIFE_GROUND_TRUTH_FILENAME))
+	check_filenames = []
 
 	for index in ground_truth_df.index:
 		date, month = ground_truth_df["Date"][index].split("-")
@@ -38,14 +60,32 @@ def main():
 
 		create_directory(mobile_dataset_output_directory)
 
-		# IMG_2023_08_14_16_23_51_361_RGB_(Pr_ID_1).png
+		# IMG_2023_08_14_16_16_27_916_RGB_(Pr_ID_0).png
 		mobile_rgbfilename = "IMG_2023_%s_%s_*_RGB_(%s_ID_%s).png" % (months[month], date, fruit_name_short, mobile_fid)
 		mobile_nirfilename = "IMG_2023_%s_%s_*_NIR_(%s_ID_%s).png" % (months[month], date, fruit_name_short, mobile_fid)
-		for hs_filename, mobile_rgb, mobile_nir in zip(hs_filenames, glob(os.path.join(mobile_dataset_input_directory, mobile_rgbfilename)), glob(os.path.join(mobile_dataset_input_directory, mobile_nirfilename))):
-			copy2(mobile_rgb, os.path.join(mobile_dataset_output_directory, "%s_RGB.png" % hs_filename))
-			copy2(mobile_nir, os.path.join(mobile_dataset_output_directory, "%s_NIR.png" % hs_filename))
-			print("Copied %s -> %s" % (mobile_rgb.split("/")[-1], "%s_RGB.png" % hs_filename))
-			print("Copied %s -> %s" % (mobile_nir.split("/")[-1], "%s_NIR.png" % hs_filename))
+
+		# Checks if the files captured on Mobile have correct names. Soft Assertion/Warning
+		rgb_glob_len = len(glob(os.path.join(mobile_dataset_input_directory, mobile_rgbfilename)))
+		nir_glob_len = len(glob(os.path.join(mobile_dataset_input_directory, mobile_nirfilename)))
+		if (rgb_glob_len != 2 or nir_glob_len != 2):
+			print("\t\t\tCheck file numbers for Date %s-%s, FID: %s, Fruit Short Name: %s" % (date, month, mobile_fid, fruit_name_short))
+			check_filenames.append("Date %s-%s, FID: %s, Fruit Short Name: %s" % (date, month, mobile_fid, fruit_name_short))
+
+		for hs_filename, mobile_rgb, mobile_nir in zip(hs_filenames, sorted(glob(os.path.join(mobile_dataset_input_directory, mobile_rgbfilename))), sorted(glob(os.path.join(mobile_dataset_input_directory, mobile_nirfilename)))):
+			rgb_image = imageio.imread(mobile_rgb)
+			nir_image = imageio.imread(mobile_nir)
+			rgb_image_aligned = fixedAlign(rgb_image)
+			plot_title = "Date %s-%s, FID: %s, Fruit Short Name: %s" % (date, month, mobile_fid, fruit_name_short)
+			viewImages(rgb_image, nir_image, rgb_image_aligned, plot_title, dataset_name, hs_filename)
+
+			print("Copied %s -> %s [Glob Len: %d]" % (mobile_rgb.split("/")[-1], "%s_RGB.png" % hs_filename, rgb_glob_len))
+			print("Copied %s -> %s [Glob Len: %d]" % (mobile_nir.split("/")[-1], "%s_NIR.png" % hs_filename, nir_glob_len))
+
+			imageio.imwrite(os.path.join(mobile_dataset_output_directory, "%s_RGB.png" % hs_filename), rgb_image_aligned)
+			imageio.imwrite(os.path.join(mobile_dataset_output_directory, "%s_NIR.png" % hs_filename), nir_image)
+
+	if len(check_filenames) != 0:
+		print("Check These Filenames:", check_filenames)
 
 if __name__ == "__main__":
 	main()
