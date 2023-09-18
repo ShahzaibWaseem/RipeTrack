8f6f6777eeefddf85574c2f5db40c198081bce92
Shahzaib Waseem <msw10@sfu.ca>
2023-09-18 14:43:56 -0700

    fixing inference file to new standards.

--- inference.py
+++ inference.py
@@ -4,114 +4,101 @@ import os
 import time
 import numpy as np
 from glob import glob
+from imageio import imread
 
 import torch
 from torchsummary import summary
+from models.MST import MST_Plus_Plus
 
-from models.model import Network
-from models.resblock import ResNeXtBottleneck
-
-from train import get_required_transforms
-from dataset import read_image
-from utils import save_matv73, reconstruction, load_mat, initialize_logger
+from utils import save_mat, load_mat, initialize_logger, visualize_gt_pred_hs_data, get_best_checkpoint
 from loss import test_mrae, test_rrmse, test_msam, test_sid, test_psnr, test_ssim
-from config import ILLUMINATIONS, TEST_ROOT_DATASET_DIR, TEST_DATASETS, MODEL_PATH, MODEL_NAME, APPLICATION_NAME, BAND_SPACING, RGBN_BANDS, NUMBER_OF_BANDS, BANDS, model_run_title, checkpoint_file, create_directory, init_directories
-
-import matplotlib.pyplot as plt
+from config import MODEL_PATH, TEST_ROOT_DATASET_DIR, TEST_DATASETS, APPLICATION_NAME, BANDS, MOBILE_DATASET_DIR_NAME, RECONSTRUCTED_HS_DIR_NAME, GT_RGBN_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, EPS, model_run_title, checkpoint_file, device, create_directory
 
 def calculate_metrics(img_pred, img_gt):
 	mrae = test_mrae(img_pred, img_gt)
 	rrmse = test_rrmse(img_pred, img_gt)
 	msam = test_msam(img_pred, img_gt)
 	sid = test_sid(img_pred, img_gt)
-	psnr = test_psnr(img_pred, img_gt)
-	ssim = test_ssim(img_pred, img_gt)
+	psnr = test_psnr(img_pred, img_gt, max_p=1)		# max_p = 1 for 0-1 normalized images
+	ssim = test_ssim(img_pred, img_gt, max_p=1)		# max_p = 1 for 0-1 normalized images
 	return mrae, rrmse, msam, sid, psnr, ssim
 
-def inference(model, rgbn_from_cube=True):
-	input_transform, label_transform = get_required_transforms(task="reconstruction")
+def inference(model, checkpoint_filename, mobile_reconstruction=False):
+	# input_transform, label_transform = get_required_transforms(task="reconstruction")
 	eps = 1e-5
 	logger = initialize_logger(filename="test.log")
 	log_string = "[%15s] Time: %0.9f, MRAE: %0.9f, RRMSE: %0.9f, SAM: %0.9f, SID: %0.9f, PSNR: %0.9f, SSIM: %0.9f"
-	TEST_DATASET_DIR = os.path.join(TEST_ROOT_DATASET_DIR, "working_%s" % APPLICATION_NAME)
+	TEST_DATASET_DIR = os.path.join(TEST_ROOT_DATASET_DIR, APPLICATION_NAME)
 
 	for test_dataset in TEST_DATASETS:
-		for illumination in ILLUMINATIONS:
-			print("\n" + model_run_title)
-			logger.info(model_run_title)
-			print("\nDataset: %s\nIllumination: %s\nModel: %s\n" % (APPLICATION_NAME, illumination, checkpoint_file))
-			logger.info("Dataset: %s\tIllumination: %s\tModel: %s\n" % (APPLICATION_NAME, illumination, checkpoint_file))
+		directory = os.path.join(TEST_DATASET_DIR, "%s_204ch" % test_dataset)
+		OUT_PATH = os.path.join(directory, RECONSTRUCTED_HS_DIR_NAME) if not mobile_reconstruction else os.path.join(directory, MOBILE_RECONSTRUCTED_HS_DIR_NAME)
+		create_directory(OUT_PATH)
 
-			GT_PATH = os.path.join(TEST_DATASET_DIR, "working_%s_204ch" % test_dataset)
-			IMG_PATH = os.path.join(os.path.dirname(TEST_DATASET_DIR), "RGBNIRImages", "working_%s" % APPLICATION_NAME, "working_%s_204ch" % test_dataset) if not rgbn_from_cube else None
-			INF_PATH = os.path.join(GT_PATH, "inference")
+		print("\n" + model_run_title)
+		logger.info(model_run_title)
 
-			for mat_filepath in sorted(glob(os.path.join(TEST_DATASET_DIR, "working_%s_204ch" % test_dataset, "*.mat"))):
-				start_time = time.time()
-				label = mat_filepath.split("/")[-2]
+		if mobile_reconstruction:
+			print("Mobile Reconstruction")
+			logger.info("Mobile Reconstruction") if mobile_reconstruction else None
 
-				hypercube = load_mat(mat_filepath)
-				mat_filename = "_".join(mat_filepath.split("/")[-1].split("_")[0:2]) if illumination == "cfl_led" \
-							   else mat_filepath.split("/")[-1].split("_")[0]
+		print("Dataset: %s\nTest Directory: %s\nModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
+		logger.info("Dataset: %s\tTest Directory: %s\tModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
 
-				if not rgbn_from_cube:
-					rgb_filename = mat_filename.split(".")[0] + "_RGB.png"
-					nir_filename = mat_filename.split(".")[0] + "_NIR.png"
-					rgb_filepath = os.path.join(IMG_PATH, rgb_filename)
-					nir_filepath = os.path.join(IMG_PATH, nir_filename)
-					image = read_image(rgb_filepath, nir_filepath)
-					image = np.transpose(image, [2, 0, 1])
-				else:
-					image = hypercube[:, :, RGBN_BANDS]
-					image = np.transpose(image, [2, 0, 1])
+		for mat_filepath in sorted(glob(os.path.join(directory, "*.mat"))):
+			start_time = time.time()
+			mat_filename = os.path.split(mat_filepath)[-1]
 
-				# Image Min: -1.1055755615234375, Hypercube Min: -1.3652015924453735, Inference Min: -1.7359950542449951
-				image = torch.from_numpy(np.expand_dims(image, axis=0)).float()
-				image = input_transform(image).cuda()
-				image = image + 1.1055755615234375 + 0.1
+			hypercube = load_mat(mat_filepath)
+			hypercube = hypercube[:, :, BANDS]
+			hypercube = (hypercube - hypercube.min()) / (hypercube.max() - hypercube.min())
+			hypercube = hypercube + EPS
 
-				hypercube = hypercube[:, :, BANDS]
-				hypercube = np.transpose(hypercube, [2, 0, 1])
-				hypercube = torch.from_numpy(hypercube).float()
-				hypercube = label_transform(hypercube)
-				hypercube = np.transpose(hypercube.numpy(), [1, 2, 0])
-				hypercube = hypercube + 1.3652015924453735 + 0.1
+			rgb_filename = mat_filename.replace(".mat", "_RGB.png")
+			nir_filename = mat_filename.replace(".mat", "_NIR.png")
 
-				# hypercube_pred = (reconstruction(image, model) + np.flip(reconstruction(np.flip(image, 2).copy(), model), 1))
-				hypercube_pred = model(image)
-				hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0])
+			rgb_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, rgb_filename))
+			nir_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, nir_filename))
+			nir_image = np.expand_dims(np.asarray(nir_image), axis=-1)
 
-				# fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 5))
-				# ax1.imshow(np.transpose(image.detach().cpu().numpy().squeeze(0), [1, 2, 0])[:, :, :3])
-				# ax1.set_title("Input")
-				# ax2.imshow(hypercube[:, :, 50])
-				# ax2.set_title("Ground Truth")
-				# ax3.imshow(hypercube_pred[:, :, 50])
-				# ax3.set_title("Output")
-				# plt.show()
+			rgb_image = (rgb_image - rgb_image.min()) / (rgb_image.max() - rgb_image.min())
+			nir_image = (nir_image - nir_image.min()) / (nir_image.max() - nir_image.min())
 
-				end_time = time.time() - start_time
+			rgb_image = np.transpose(rgb_image, [2, 0, 1])
+			nir_image = np.transpose(nir_image, [2, 0, 1])
 
-				create_directory(INF_PATH)
+			image = torch.from_numpy(np.expand_dims(np.concatenate((rgb_image, nir_image), axis=0), axis=0)).float().to(device)
 
-				inf_mat_name = os.path.join(TEST_DATASET_DIR, label, "inference", "inf_" + mat_filename)
-				save_matv73(inf_mat_name, hypercube_pred)
+			hypercube_pred = model(image)
+			hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0])
+			# hypercube_pred = hypercube_pred + EPS			# should work without this line but just in case
 
-				mrae, rrmse, msam, sid, psnr, ssim = calculate_metrics(hypercube_pred+eps, hypercube+eps)
+			end_time = time.time() - start_time
+
+			hypercube_pred_filepath = os.path.join(OUT_PATH, mat_filename)
+			save_mat(hypercube_pred_filepath, hypercube_pred)
+
+			if not mobile_reconstruction:
+				# visualize_gt_pred_hs_data(hypercube, hypercube_pred, 12)
+				mrae, rrmse, msam, sid, psnr, ssim = calculate_metrics(hypercube_pred, hypercube)
 
 				print(log_string % (mat_filename, end_time, mrae, rrmse, msam, sid, psnr, ssim))
 				logger.info(log_string % (mat_filename, end_time, mrae, rrmse, msam, sid, psnr, ssim))
+			else:
+				print("[%15s] Time: %0.9f" % (mat_filename, end_time))
+				logger.info("[%15s] Time: %0.9f" % (mat_filename, end_time))
 
 def main():
-	save_point = torch.load(os.path.join(MODEL_PATH, checkpoint_file))
-	model_param = save_point["state_dict"]
-	model = Network(ResNeXtBottleneck, block_num=10, input_channel=4, n_hidden=64, output_channel=len(BANDS))
+	# checkpoint_filename, epoch, iter, model_param, optimizer, val_loss, val_acc = get_best_checkpoint(task="reconstruction")
+	checkpoint_filename = checkpoint_file
+	checkpoint = torch.load(os.path.join(MODEL_PATH, "reconstruction", checkpoint_file))
+	model_param = checkpoint["state_dict"]
+	model = MST_Plus_Plus(in_channels=4, out_channels=len(BANDS), n_feat=len(BANDS), stage=3)
 	model.load_state_dict(model_param)
 	model = model.cuda()
 	model.eval()
 	print(summary(model=model, input_data=(4, 512, 512)))
-	inference(model, rgbn_from_cube=False)
+	inference(model, checkpoint_filename, mobile_reconstruction=True)
 
 if __name__ == "__main__":
-	# init_directories()
 	main()
