f050ec5b7a2b63062e0d3a7b058f75f323457a71
Shahzaib Waseem <msw10@sfu.ca>
2024-01-29 20:58:40 -0800

    Patching, augmentation, illumination changes

--- dataset.py
+++ dataset.py
@@ -3,7 +3,9 @@ import time
 import random
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 
+import cv2
 import h5py
 from glob import glob
 from imageio import imread
@@ -13,8 +15,8 @@ from torchvision import transforms
 from torchsampler import ImbalancedDatasetSampler
 from torch.utils.data import Dataset, DataLoader, random_split, WeightedRandomSampler, Subset
 
-from utils import load_mat, read_image, data_augmentation, get_normalization_parameters, visualize_data_item
-from config import use_mobile_dataset, APPEND_SECONDARY_RGB_CAM_INPUT, MOBILE_DATASET_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, BAND_SPACING, RGBN_BANDS, BANDS, BANDS_WAVELENGTHS, TEST_DATASETS, GT_RGBN_DIR_NAME, TRAIN_DATASET_DIR, TRAIN_DATASET_FILES, VALID_DATASET_FILES, TEST_ROOT_DATASET_DIR, APPLICATION_NAME, IMAGE_SIZE, PATCH_SIZE, RECONSTRUCTED_HS_DIR_NAME, EPS, DATA_PREP_PATH, MOBILE_DATASET_CROPS_FILENAME, SHELF_LIFE_GROUND_TRUTH_FILENAME, LABELS_DICT, SUB_LABELS_DICT, TIME_LEFT_DICT, FRUITS_DICT, batch_size, device
+from utils import load_mat, load_mat_patched, read_image, data_augmentation, get_normalization_parameters, visualize_data_item
+from config import use_mobile_dataset, APPEND_SECONDARY_RGB_CAM_INPUT, MOBILE_DATASET_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, PATCHED_HS_DIR_NAME, PATCHED_INFERENCE, BAND_SPACING, RGBN_BANDS, NIR_BANDS, BANDS, BANDS_WAVELENGTHS, TEST_DATASETS, GT_RGBN_DIR_NAME, TRAIN_DATASET_DIR, TRAIN_DATASET_FILES, VALID_DATASET_FILES, TEST_ROOT_DATASET_DIR, APPLICATION_NAME, IMAGE_SIZE, PATCH_SIZE, CLASSIFICATION_PATCH_SIZE, STRIDE, RECONSTRUCTED_HS_DIR_NAME, EPS, DATA_PREP_PATH, GT_DATASET_CROPS_FILENAME, MOBILE_DATASET_CROPS_FILENAME, SHELF_LIFE_GROUND_TRUTH_FILENAME, LABELS_DICT, SUB_LABELS_DICT, TIME_LEFT_DICT, FRUITS_DICT, batch_size, device
 
 def get_dataloaders(input_transform, label_transform, task, load_from_h5=False, trainset_size=0.7):
 	if load_from_h5:
@@ -375,11 +377,13 @@ class DatasetFromDirectory(Dataset):
 		return image, hypercube, classlabel, actual_classlabel
 
 def get_dataloaders_reconstruction(trainset_size=0.7):
+	# reconstructionTransforms = transforms.Compose([Misalign(movePixels=32, imageSize=IMAGE_SIZE, newImageSize=448)])
 	dataset = DatasetFromDirectoryReconstruction(
 		root=TEST_ROOT_DATASET_DIR,
 		application_name=APPLICATION_NAME,
 		patch_size=PATCH_SIZE,
 		append_secondary_input=APPEND_SECONDARY_RGB_CAM_INPUT,
+		transforms=None,
 		verbose=True
 	)
 	train_data, valid_data = random_split(dataset, [int(trainset_size*len(dataset)), len(dataset) - int(len(dataset)*trainset_size)])
@@ -417,12 +421,14 @@ class DatasetFromDirectoryReconstruction(Dataset):
 			datapoints += len(hs_path) * ((image_width // patch_size) * (image_height // patch_size))
 		return datapoints
 
-	def __init__(self, root, application_name=APPLICATION_NAME, patch_size=64, append_secondary_input=False, verbose=False):
+	def __init__(self, root, application_name=APPLICATION_NAME, patch_size=64, append_secondary_input=False, transforms=None, verbose=False):
 		self.patch_size = patch_size
 		self.append_secondary_input = append_secondary_input
-
+		illuminations_considered = ["", "-cloudy", "-day", "-night", "-sunlight"]
 		image_width, image_height = IMAGE_SIZE, IMAGE_SIZE
 		rgbn_counter, hypercube_counter, patch_index, data_idx = 0, 0, 0, 0
+		self.transforms = transforms
+		movePixels = 32
 		# datapoints = self.pre_calculate_number_of_datapoints(os.path.join(root, application_name), TEST_DATASETS, patch_size)		# Datapoints: 51584
 		# self.rgb_images = np.empty(shape=(datapoints, 3, patch_size, patch_size))
 		# self.nir_images = np.empty(shape=(datapoints, 1, patch_size, patch_size))
@@ -435,25 +441,31 @@ class DatasetFromDirectoryReconstruction(Dataset):
 			print(" " * 19, "{0:62}".format(directory), "{} and {}".format(GT_RGBN_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME) if append_secondary_input else GT_RGBN_DIR_NAME, end="\t") if verbose else None
 			dataset_load_time = time.time()
 			for filename in glob(os.path.join(directory, "*.mat")):
+				image_width, image_height = IMAGE_SIZE, IMAGE_SIZE
 				hypercube = load_mat(filename)
+				nir_image = hypercube[:, :, random.choice(NIR_BANDS)]
 				hypercube = hypercube[:, :, BANDS]
+				hypercube = hypercube[movePixels:image_width-movePixels, movePixels:image_height-movePixels, :] if not self.transforms == None else hypercube
 				hypercube = (hypercube - hypercube.min()) / (hypercube.max() - hypercube.min())
 				hypercube = np.transpose(hypercube, [2, 0, 1]) + EPS
 				hypercube = np.expand_dims(hypercube, axis=0)
 
-				rgb_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_RGB.png")))
+				rgb_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_RGB%s.png" % random.choice(illuminations_considered))))
+				rgb_image = self.transforms(rgb_image) if not self.transforms == None else rgb_image
 				rgb_image = (rgb_image - rgb_image.min()) / (rgb_image.max() - rgb_image.min())
 				rgb_image = np.transpose(rgb_image, [2, 0, 1])
 				rgb_image = np.expand_dims(rgb_image, axis=0)
 
-				nir_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_NIR.png")))
+				# nir_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_NIR.png")))
+				nir_image = nir_image[movePixels:image_width-movePixels, movePixels:image_height-movePixels] if not self.transforms == None else nir_image
 				nir_image = (nir_image - nir_image.min()) / (nir_image.max() - nir_image.min())
 				nir_image = np.expand_dims(np.asarray(nir_image), 2)
 				nir_image = np.transpose(nir_image, [2, 0, 1])
 				nir_image = np.expand_dims(nir_image, axis=0)
 
 				if append_secondary_input:
-					secondary_rgb_image = imread(os.path.join(directory, GT_SECONDARY_RGB_CAM_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_RGB.png")))
+					secondary_rgb_image = imread(os.path.join(directory, GT_SECONDARY_RGB_CAM_DIR_NAME, os.path.split(filename)[-1].replace(".mat", "_RGB%s.png" % random.choice(illuminations_considered))))
+					secondary_rgb_image = self.transforms(secondary_rgb_image) if not self.transforms == None else secondary_rgb_image
 					secondary_rgb_image = (secondary_rgb_image - secondary_rgb_image.min()) / (secondary_rgb_image.max() - secondary_rgb_image.min())
 					secondary_rgb_image = np.transpose(secondary_rgb_image, [2, 0, 1])
 					secondary_rgb_image = np.expand_dims(secondary_rgb_image, axis=0)
@@ -467,7 +479,7 @@ class DatasetFromDirectoryReconstruction(Dataset):
 							self.nir_images = nir_image[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size]
 							self.hypercubes = hypercube[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size]
 							self.secondary_rgb_images = secondary_rgb_image[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size] if self.append_secondary_input else None
-						
+
 						self.rgb_images = np.append(self.rgb_images, rgb_image[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size], axis=0)
 						self.nir_images = np.append(self.nir_images, nir_image[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size], axis=0)
 						self.hypercubes = np.append(self.hypercubes, hypercube[:, :, patch_i:patch_i+self.patch_size, patch_j:patch_j+self.patch_size], axis=0)
@@ -512,8 +524,8 @@ class DatasetFromDirectoryReconstruction(Dataset):
 		hypercube = self.hypercubes[index]
 		secondary_rgb_image = self.secondary_rgb_images[index] if self.append_secondary_input else None
 
-		use_secondary = bool(random.getrandbits(1)) if self.append_secondary_input else False
-		image = np.concatenate((secondary_rgb_image, nir_image), axis=0) if use_secondary else np.concatenate((rgb_image, nir_image), axis=0)
+		# use_secondary = bool(random.getrandbits(1)) if self.append_secondary_input else False
+		image = np.concatenate((secondary_rgb_image, nir_image), axis=0) if self.append_secondary_input else np.concatenate((rgb_image, nir_image), axis=0)
 
 		image = torch.Tensor(image).float()
 		hypercube = torch.Tensor(hypercube).float()
@@ -524,7 +536,19 @@ class DatasetFromDirectoryReconstruction(Dataset):
 
 		return image, hypercube
 
-from collections import OrderedDict
+class Misalign(object):
+	def __init__(self, movePixels=32, imageSize=512, newImageSize=448):
+		self.movePixels = movePixels
+		self.imageSize = imageSize
+		self.newImageSize = newImageSize
+		self.startingPixel = (self.imageSize - self.newImageSize) // 2
+
+	def __call__(self, image):
+		misaligned = image.copy()
+		moveXpixels = random.randint(-self.movePixels, self.movePixels)
+		moveYpixels = random.randint(-self.movePixels, self.movePixels)
+		misaligned = misaligned[self.startingPixel+moveXpixels:self.startingPixel+moveXpixels+self.newImageSize, self.startingPixel+moveYpixels:self.startingPixel+moveYpixels+self.newImageSize, :]
+		return misaligned
 
 class_sizes = OrderedDict([(label, 0) for label in LABELS_DICT.keys()])
 subclass_sizes = OrderedDict([(label, 0) for label in TIME_LEFT_DICT.values()])
@@ -539,6 +563,7 @@ def get_dataloaders_classification(trainset_size=0.7):
 		application_name=APPLICATION_NAME,
 		mobile_reconstructed_folder=MOBILE_RECONSTRUCTED_HS_DIR_NAME if use_mobile_dataset else None,
 		transforms=classificationTransforms,
+		patched_inference=PATCHED_INFERENCE,
 		verbose=True
 	)
 	train_indices, valid_indices = dataset.divide_train_test(trainset_size)
@@ -572,16 +597,16 @@ def get_dataloaders_classification(trainset_size=0.7):
 class DatasetFromDirectoryClassification(Dataset):
 	images, hypercubes, labels, sublabels, fruits = [], [], [], [], []
 
-	def __init__(self, root, application_name=APPLICATION_NAME, mobile_reconstructed_folder=None, transforms=None, verbose=False):
+	def __init__(self, root, application_name=APPLICATION_NAME, mobile_reconstructed_folder=None, patched_inference=True, transforms=None, verbose=False):
 		global class_sizes, subclass_sizes
 		self.transforms = transforms
 
 		image_width, image_height = 512, 512
-		self.patch_size = 45
-		self.stride = 10
+		self.patch_size = CLASSIFICATION_PATCH_SIZE
+		self.stride = STRIDE
 		hypercube_counter = 0
 
-		crops_df = pd.read_csv(os.path.join(DATA_PREP_PATH, MOBILE_DATASET_CROPS_FILENAME))
+		crops_df = pd.read_csv(os.path.join(DATA_PREP_PATH, MOBILE_DATASET_CROPS_FILENAME if mobile_reconstructed_folder else GT_DATASET_CROPS_FILENAME))
 		shelflife_df = pd.read_csv(os.path.join(DATA_PREP_PATH, SHELF_LIFE_GROUND_TRUTH_FILENAME))
 		crops_df["w"] = crops_df["xmax"] - crops_df["xmin"]
 		crops_df["h"] = crops_df["ymax"] - crops_df["ymin"]
@@ -595,9 +620,10 @@ class DatasetFromDirectoryClassification(Dataset):
 			print("{0:21}".format(os.path.split(directory)[-1] if mobile_reconstructed_folder == None else dataset), end=":")
 			fruit_name_capt = shelflife_df["Fruit"].str.contains(dataset.split("-")[0].capitalize())
 			friut_type_capt = shelflife_df["Type"].str.contains(dataset.split("-")[1].capitalize())
-			sub_labels_print_dict = shelflife_df[fruit_name_capt & friut_type_capt]["Time Left"].value_counts()[shelflife_df["Time Left"].unique()].to_dict()
+			# sub_labels_print_dict = shelflife_df[fruit_name_capt & friut_type_capt]["Remaining Life"].value_counts()b[shelflife_df["Remaining Life"].unique()].to_dict()
 			print(shelflife_df[fruit_name_capt & friut_type_capt]["Shelf Life Label"].value_counts()[shelflife_df["Shelf Life Label"].unique()].to_dict(), end=", ")
-			print({list(TIME_LEFT_DICT.keys())[key]: value for key, value in sub_labels_print_dict.items()}, end=", ")
+			# print({list(TIME_LEFT_DICT.keys()).index(key): value for key, value in sub_labels_print_dict.items()}, end=", ")
+			# print(sub_labels_print_dict, end=", ")
 			dataset_load_time = time.time()
 			for filename in glob(os.path.join(directory, "*.mat")):
 				hypercube_number = os.path.split(filename)[-1].split(".")[0].split("_")[0]
@@ -630,9 +656,9 @@ class DatasetFromDirectoryClassification(Dataset):
 				ymax = int(crop_record["ymax"].iloc[0])
 
 				label_name = shelflife_record["Shelf Life Label"]
-				sublabel_name = shelflife_record["Time Left"]
+				sublabel_name = shelflife_record["Remaining Life"]
 				label = LABELS_DICT.get(label_name)
-				sublabel = sublabel_name
+				sublabel = TIME_LEFT_DICT.get(sublabel_name)
 				fruit_name = "{} {}".format(shelflife_record["Fruit"], shelflife_record["Type"])
 
 				for patch_i in range(xmin, xmax, self.stride):
@@ -652,7 +678,20 @@ class DatasetFromDirectoryClassification(Dataset):
 						self.sublabels.append(sublabel)
 						self.fruits.append(fruit_name)
 						class_sizes[label_name] += 1
-						subclass_sizes[sublabel_name] += 1
+						subclass_sizes[sublabel] += 1
+
+				if patched_inference:
+					patched_filename = os.path.join(directory, PATCHED_HS_DIR_NAME, os.path.split(filename)[-1])
+					hypercube_combined = load_mat_patched(patched_filename)
+					for hypercube in hypercube_combined.values():
+						hypercube = np.transpose(hypercube, [2, 0, 1]) + EPS
+						self.hypercubes.append(hypercube)
+						self.labels.append(label)
+						self.sublabels.append(sublabel)
+						self.fruits.append(fruit_name)
+						class_sizes[label_name] += 1
+						subclass_sizes[sublabel] += 1
+
 				hypercube_counter += 1
 			print("{:>3}s".format(round(time.time()-dataset_load_time)))
 
@@ -664,6 +703,7 @@ class DatasetFromDirectoryClassification(Dataset):
 			print("Number of Bands:".ljust(40), len(BANDS))
 			print("Number of Hypercubes Files:".ljust(40), hypercube_counter)
 			# print("RGB+NIR Dataset Size:".ljust(40), len(self.images))
+			print("Using Patched Hypercubes:".ljust(40), patched_inference)
 			print("Hypercube Dataset Size:".ljust(40), len(self.hypercubes))
 			print("Labels Dataset Size:".ljust(40), len(self.labels))
 			print("Sublabels Dataset Size:".ljust(40), len(self.sublabels))
@@ -699,8 +739,8 @@ class DatasetFromDirectoryClassification(Dataset):
 		hypercube = self.hypercubes[index]
 		if self.transforms is not None and index in self.train_indices:
 			hypercube = self.transforms(hypercube)
-		label = self.labels[index]
-		sublabel = self.sublabels[index]
+		label = torch.tensor(self.labels[index])
+		sublabel = torch.tensor(self.sublabels[index])
 		fruit = self.fruits[index]
 
 		# visualize_data_item(None, hypercube, None, 12, "0")
