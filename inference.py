fix for patched inference save storage overflow

inference.py
@@ -13,7 +13,7 @@ from models.MST import MST_Plus_Plus
 
 from utils import save_mat, save_mat_patched, load_mat, load_mat_patched, initialize_logger, visualize_gt_pred_hs_data, get_best_checkpoint
 from loss import test_mrae, test_rrmse, test_msam, test_sid, test_psnr, test_ssim
-from config import MODEL_PATH, TEST_ROOT_DATASET_DIR, TEST_DATASETS, APPLICATION_NAME, BANDS, CLASSIFICATION_PATCH_SIZE, STRIDE, DATA_PREP_PATH, GT_DATASET_CROPS_FILENAME, MOBILE_DATASET_CROPS_FILENAME, MOBILE_DATASET_DIR_NAME, RECONSTRUCTED_HS_DIR_NAME, GT_RGBN_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, PATCHED_INFERENCE, PATCHED_HS_DIR_NAME, EPS, model_run_title, checkpoint_file, device, create_directory
+from config import MODEL_PATH, TEST_ROOT_DATASET_DIR, TEST_DATASETS, APPLICATION_NAME, BANDS, CLASSIFICATION_PATCH_SIZE, STRIDE, DATA_PREP_PATH, GT_DATASET_CROPS_FILENAME, MOBILE_DATASET_CROPS_FILENAME, MOBILE_DATASET_DIR_NAME, RECONSTRUCTED_HS_DIR_NAME, GT_RGBN_DIR_NAME, GT_SECONDARY_RGB_CAM_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, PATCHED_INFERENCE, PATCHED_HS_DIR_NAME, EPS, var_name, model_run_title, checkpoint_file, device, create_directory
 
 def calculate_metrics(img_pred, img_gt):
 	mrae = test_mrae(img_pred, img_gt)
@@ -54,6 +54,7 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 
 		print("Dataset: %s\nTest Directory: %s\nModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
 		logger.info("Dataset: %s\tTest Directory: %s\tModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
+		p_mrae, p_rrmse, p_msam, p_sid, p_psnr, p_ssim, num_patches = 0, 0, 0, 0, 0, 0, 0
 
 		for mat_filepath in sorted(glob(os.path.join(directory, "*.mat"))):
 			start_time = time.time()
@@ -63,31 +64,39 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 			hypercube = load_mat(mat_filepath)
 			hypercube = hypercube[:, :, BANDS]
 			hypercube = (hypercube - hypercube.min()) / (hypercube.max() - hypercube.min())
+			# hypercube = np.transpose(hypercube, [2, 0, 1]) + EPS
+
 			min_hc, max_hc = min(min_hc, hypercube.min()), max(max_hc, hypercube.max())
-			hypercube = np.maximum(np.minimum(hypercube, 1.0), 0.0)
+			# hypercube = np.maximum(np.minimum(hypercube, 1.0), 0.0)
 			hypercube = hypercube + EPS
+			hypercube = np.asarray(hypercube)
 
 			rgb_filename = mat_filename.replace(".mat", "_RGB%s.png" % "-D")
-			rgb_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, rgb_filename))
+			rgb_image = imread(os.path.join(directory, GT_SECONDARY_RGB_CAM_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, rgb_filename))
 			rgb_image = (rgb_image - rgb_image.min()) / (rgb_image.max() - rgb_image.min())
 			rgb_image = np.transpose(rgb_image, [2, 0, 1])
 			rgb_image = np.expand_dims(rgb_image, axis=0)
+			rgb_image = np.asarray(rgb_image)
 
 			nir_filename = mat_filename.replace(".mat", "_NIR.png")
-			nir_image = imread(os.path.join(directory, GT_RGBN_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, nir_filename))
+			nir_image = imread(os.path.join(directory, GT_SECONDARY_RGB_CAM_DIR_NAME if not mobile_reconstruction else MOBILE_DATASET_DIR_NAME, nir_filename))
 			nir_image = (nir_image - nir_image.min()) / (nir_image.max() - nir_image.min())
 			nir_image = np.expand_dims(np.asarray(nir_image), axis=-1)
 			nir_image = np.transpose(nir_image, [2, 0, 1])
 			nir_image = np.expand_dims(nir_image, axis=0)
+			nir_image = np.asarray(nir_image)
+
 			image = np.concatenate((rgb_image, nir_image), axis=1)
 			image_tensor = torch.Tensor(image).float().to(device)
 
 			with torch.no_grad():
 				hypercube_pred = model(image_tensor)
 			hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0])
+			# hypercube_pred = hypercube_pred.squeeze(0).cpu().detach().numpy()
 			hypercube_pred = (hypercube_pred - hypercube_pred.min()) / (hypercube_pred.max() - hypercube_pred.min())
 			min_phc, max_phc = min(min_phc, hypercube_pred.min()), max(max_phc, hypercube_pred.max())
-			hypercube_pred = np.maximum(np.minimum(hypercube_pred, 1.0), 0.0)
+			
+			# hypercube_pred = np.maximum(np.minimum(hypercube_pred, 1.0), 0.0)
 			hypercube_pred = hypercube_pred + EPS			# should work without this line but just in case
 
 			end_time = time.time() - start_time
@@ -111,19 +120,39 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 				ymin = int(crop_record["ymin"].iloc[0])
 				xmax = int(crop_record["xmax"].iloc[0])
 				ymax = int(crop_record["ymax"].iloc[0])
+				# hypercube_joined = hypercube.copy()
 
-				for patch_i in range(xmin, xmax, STRIDE):
+				for patch_i in range(xmin, xmax, CLASSIFICATION_PATCH_SIZE):
 					if patch_i+CLASSIFICATION_PATCH_SIZE > xmax: continue
-					for patch_j in range(ymin, ymax, STRIDE):
+					for patch_j in range(ymin, ymax, CLASSIFICATION_PATCH_SIZE):
 						if patch_j+CLASSIFICATION_PATCH_SIZE > ymax: continue
 						imageCrop = image[:, :, patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE]
+						hypercubeCrop = hypercube[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :]
+						# hypercubeCrop = np.transpose(hypercubeCrop, [1, 2, 0])
 						if (imageCrop.shape[2:4] != (CLASSIFICATION_PATCH_SIZE, CLASSIFICATION_PATCH_SIZE)):
+							# hypercube_joined[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :] = 0
 							continue
 						image_tensor = torch.Tensor(imageCrop).float().to(device)
 						with torch.no_grad():
 							hypercube_pred = model(image_tensor)
-						hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0])
-						hypercube_combined[u"(%d, %d)" % (patch_i, patch_j)] = hypercube_pred
+						# hypercube_pred = hypercube_pred.squeeze(0).cpu().detach().numpy()
+						hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0]) + EPS
+						if not mobile_reconstruction:
+							mrae, rrmse, msam, sid, psnr, ssim = calculate_metrics(hypercube_pred + EPS, hypercubeCrop)
+							p_mrae += mrae
+							p_rrmse += rrmse
+							p_msam += msam
+							p_sid += sid
+							p_psnr += psnr
+							p_ssim += ssim
+							num_patches += 1
+
+						# hypercube_joined[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :] = hypercube_pred
+						# patches.append((patch_i, patch_j))
+						# print(patch_i, patch_j, hypercube_pred.shape, hypercubeCrop.shape)
+
+						hypercube_combined[u"(%d, %d)"% (patch_i, patch_j)] = hypercube_pred
+				print(log_string % (mat_filename+"_patch", end_time, p_mrae/num_patches, p_rrmse/num_patches, p_msam/num_patches, p_sid/num_patches, p_psnr/num_patches, p_ssim/num_patches)) if not mobile_reconstruction else None
 				save_mat_patched(os.path.join(OUT_PATH, PATCHED_HS_DIR_NAME, mat_filename), hypercube_combined)
 		print("Min Hypercube: %0.9f, Max Hypercube: %0.9f" % (min_hc, max_hc))
 		print("Min Predicted Hypercube: %0.9f, Max Predicted Hypercube: %0.9f" % (min_phc, max_phc))
@@ -131,14 +160,14 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 def main():
 	checkpoint_filename, epoch, iter, model_param, optimizer, val_loss, val_acc = get_best_checkpoint(task="reconstruction")
 	# checkpoint_filename = checkpoint_file
-	checkpoint = torch.load(os.path.join(MODEL_PATH, "reconstruction", checkpoint_filename))
-	model_param = checkpoint["state_dict"]
+	# checkpoint = torch.load(os.path.join(MODEL_PATH, "reconstruction", checkpoint_filename))
+	# model_param = checkpoint["state_dict"]
 	model = MST_Plus_Plus(in_channels=4, out_channels=len(BANDS), n_feat=len(BANDS), stage=3)
 	model.load_state_dict(model_param)
 	model = model.to(device)
 	model.eval()
 	print(summary(model=model, input_data=(4, 512, 512)))
-	inference(model, checkpoint_filename, mobile_reconstruction=False, patched_inference=PATCHED_INFERENCE)
+	inference(model, checkpoint_filename, mobile_reconstruction=True, patched_inference=PATCHED_INFERENCE)
 
 if __name__ == "__main__":
 	main()