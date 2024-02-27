37eacdd60c2caad3c703258f7e8683638a67a576
Shahzaib Waseem <msw10@sfu.ca>
2024-02-26 16:40:15 -0800

    added folder names for reconstruction experiments

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 var_name = "hcube"					# key for the dictionary which are saved in the files
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-EPS = 0.00001
+EPS = 0.0001
 
 GT_DATASET_CROPS_FILENAME = "gtShelfLifeCrops.csv"
 MOBILE_DATASET_CROPS_FILENAME = "mobileShelfLifeCrops.csv"
@@ -26,16 +26,19 @@ SUB_LABELS_DICT = OrderedDict([("Pretty Unripe", 0), ("Almost Ripe", 1), ("Ripen
 TIME_LEFT_DICT = OrderedDict([("100%", 0), ("90%", 1), ("80%", 2), ("70%", 3), ("60%", 4), ("50%", 5), ("40%", 6), ("30%", 7), ("20%", 8), ("10%", 9), ("0%", 10)])
 
 GT_RGBN_DIR_NAME = "rgbn"
-GT_SECONDARY_RGB_CAM_DIR_NAME = "secondary-rgbn"
+GT_AUXILIARY_RGB_CAM_DIR_NAME = "secondary-rgbn"
 MOBILE_DATASET_DIR_NAME = "mobile-rgbn"
+GT_REMOVED_IR_CUTOFF_DIR_NAME = "rgbnir-sensor"
+GT_REMOVED_IR_CUTOFF_RECONSTRUCTED_DIR_NAME = "rgbnir-sensor-reconstructed"
 RECONSTRUCTED_HS_DIR_NAME = "reconstructed"
 MOBILE_RECONSTRUCTED_HS_DIR_NAME = "mobile-reconstructed"
 PATCHED_HS_DIR_NAME = "patched"
 VISUALIZATION_DIR_NAME = "visualizations"
 MOBILE_MODELS_DIR_NAME = "mobileModels"
+TRAIN_VAL_TEST_SPLIT_DIR_NAME = "split"
 
-APPEND_SECONDARY_RGB_CAM_INPUT = True
-PATCHED_INFERENCE = True
+APPEND_SECONDARY_RGB_CAM_INPUT = False
+PATCHED_INFERENCE = False
 
 PREDEF_TRANSFORMS_FILENAME = "transforms{}.pth".format("_appended" if APPEND_SECONDARY_RGB_CAM_INPUT else "")
 
@@ -64,12 +67,12 @@ NIR_BANDS = list(range(186, 195))		# correspond to NIR bands range
 # BANDS = [RGBN_BANDS, range(104, 204, 2)]
 # Bands: (450, 650, 5), (670, 690, 5), (750, 900, 5), (900, 1000, 5)
 # BANDS = [range(19, 87, 5), range(94, 102, 5), range(120, 170, 5), range(170, 204, 5)]
+# BANDS = [103, 106, 110, 113, 116, 120, 123, 126, 130, 133, 136, 140, 143, 146, 150, 153, 156, 159, 163, 166, 169, 173, 176, 179, 182, 186, 189, 192, 195, 199, 202]
 BANDS = [range(0, 204, 3)]
 BANDS_WAVELENGTHS = [397.32, 400.20, 403.09, 405.97, 408.85, 411.74, 414.63, 417.52, 420.40, 423.29, 426.19, 429.08, 431.97, 434.87, 437.76, 440.66, 443.56, 446.45, 449.35, 452.25, 455.16, 458.06, 460.96, 463.87, 466.77, 469.68, 472.59, 475.50, 478.41, 481.32, 484.23, 487.14, 490.06, 492.97, 495.89, 498.80, 501.72, 504.64, 507.56, 510.48, 513.40, 516.33, 519.25, 522.18, 525.10, 528.03, 530.96, 533.89, 536.82, 539.75, 542.68, 545.62, 548.55, 551.49, 554.43, 557.36, 560.30, 563.24, 566.18, 569.12, 572.07, 575.01, 577.96, 580.90, 583.85, 586.80, 589.75, 592.70, 595.65, 598.60, 601.55, 604.51, 607.46, 610.42, 613.38, 616.34, 619.30, 622.26, 625.22, 628.18, 631.15, 634.11, 637.08, 640.04, 643.01, 645.98, 648.95, 651.92, 654.89, 657.87, 660.84, 663.81, 666.79, 669.77, 672.75, 675.73, 678.71, 681.69, 684.67, 687.65, 690.64, 693.62, 696.61, 699.60, 702.58, 705.57, 708.57, 711.56, 714.55, 717.54, 720.54, 723.53, 726.53, 729.53, 732.53, 735.53, 738.53, 741.53, 744.53, 747.54, 750.54, 753.55, 756.56, 759.56, 762.57, 765.58, 768.60, 771.61, 774.62, 777.64, 780.65, 783.67, 786.68, 789.70, 792.72, 795.74, 798.77, 801.79, 804.81, 807.84, 810.86, 813.89, 816.92, 819.95, 822.98, 826.01, 829.04, 832.07, 835.11, 838.14, 841.18, 844.22, 847.25, 850.29, 853.33, 856.37, 859.42, 862.46, 865.50, 868.55, 871.60, 874.64, 877.69, 880.74, 883.79, 886.84, 889.90, 892.95, 896.01, 899.06, 902.12, 905.18, 908.24, 911.30, 914.36, 917.42, 920.48, 923.55, 926.61, 929.68, 932.74, 935.81, 938.88, 941.95, 945.02, 948.10, 951.17, 954.24, 957.32, 960.40, 963.47, 966.55, 969.63, 972.71, 975.79, 978.88, 981.96, 985.05, 988.13, 991.22, 994.31, 997.40, 1000.49, 1003.58]
-
 ### Hyperparamters for the model ###
 batch_size = 45
-end_epoch = 501
+end_epoch = 101
 init_lr = 0.0001
 
 ### Variables used for printing the results in the logs ###
@@ -94,10 +97,10 @@ classicication_run_title = "Model: %s\tDataset: %s\tNumber of Classes: %d\tSub C
 ### to create the checkpoint of the model ###
 checkpoint_fileprestring = "%s_%s" % (MODEL_NAME, APPLICATION_NAME)
 classification_checkpoint_fileprestring = "%s_%s" % (CLASSIFIER_MODEL_NAME, APPLICATION_NAME)
-checkpoint_file = "MSLP_%s_399.pkl" % checkpoint_fileprestring
+checkpoint_file = "MSLP_%s_100.pkl" % checkpoint_fileprestring
 # checkpoint_file = "HS_model_%d.pkl" % end_epoch
 run_pretrained = False					# if True, the model is loaded from the checkpoint_file
-use_mobile_dataset = True				# if True, the model is trained on the mobile dataset
+use_mobile_dataset = False				# if True, the model is trained on the mobile dataset
 transfer_learning = False				# if True, the model will freeze all layers except the last MST block and conv layers
 
 mobile_model_file = "model_%s.pth" % APPLICATION_NAME
