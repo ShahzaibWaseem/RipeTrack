3bedcefdfb2846eebf658dd6f9b2a2722a463345
Shahzaib Waseem <msw10@sfu.ca>
2024-02-26 16:39:42 -0800

    torch modules loss functions

--- loss.py
+++ loss.py
@@ -5,60 +5,71 @@ import numpy as np
 from skimage.metrics import structural_similarity
 
 import torch
+import torch.nn as nn
 
-def mrae_loss(tensor_pred, tensor_gt):
+class Loss_MRAE(nn.Module):
 	""" Computes the Mean Relative Absolute Error Loss (PyTorch - Training Loss) """
-	error = torch.abs((tensor_pred-tensor_gt)/tensor_gt)
-	mrae = torch.mean(error.view(-1))
-	return mrae
+	def __init__(self):
+		super(Loss_MRAE, self).__init__()
 
-def sam_loss(tensor_pred, tensor_gt):
+	def forward(self, tensor_pred, tensor_gt):
+		assert tensor_pred.shape == tensor_gt.shape
+		error = torch.abs((tensor_pred-tensor_gt)/tensor_gt)
+		mrae = torch.mean(error.reshape(-1))
+		return mrae
+
+class Loss_SAM(nn.Module):
 	""" Computes the Spectral Angle Mapper Loss (PyTorch - Training Loss) """
-	EPS = 1e-7
-	# inner product
-	dot = torch.sum(tensor_pred * tensor_gt, dim=1).view(-1)
-	# norm calculations
-	image = tensor_pred.view(-1, tensor_pred.shape[1])
-	norm_original = torch.norm(image, p=2, dim=1)
+	def __init__(self):
+		super(Loss_SAM, self).__init__()
 
-	target = tensor_gt.view(-1, tensor_gt.shape[1])
-	norm_reconstructed = torch.norm(target, p=2, dim=1)
+	def forward(self, tensor_pred, tensor_gt):
+		assert tensor_pred.shape == tensor_gt.shape
+		EPS = 1e-7
+		# inner product
+		dot = torch.sum(tensor_pred * tensor_gt, dim=1).view(-1)
+		# norm calculations
+		image = tensor_pred.reshape(-1, tensor_pred.shape[1])
+		norm_original = torch.norm(image, p=2, dim=1)
 
-	norm_product = (norm_original.mul(norm_reconstructed)).pow(-1)
-	argument = dot.mul(norm_product)
-	# for avoiding arccos(1)
-	acos = torch.acos(torch.clamp(argument, min=-1+EPS, max=1-EPS))
-	loss = torch.mean(acos)
+		target = tensor_gt.reshape(-1, tensor_gt.shape[1])
+		norm_reconstructed = torch.norm(target, p=2, dim=1)
 
-	if torch.isnan(loss):
-		raise ValueError(f"Loss is NaN value. Consecutive values - dot: {dot},\
-			norm original: {norm_original}, norm reconstructed: {norm_reconstructed},\
-			norm product: {norm_product}, argument: {argument}, acos: {acos},\
-			loss: {loss}, input: {tensor_pred}, output: {target}")
-	return loss
+		norm_product = (norm_original.mul(norm_reconstructed)).pow(-1)
+		argument = dot.mul(norm_product)
+		# for avoiding arccos(1)
+		acos = torch.acos(torch.clamp(argument, min=-1+EPS, max=1-EPS))
+		loss = torch.mean(acos)
 
-def sid_loss(tensor_pred, tensor_gt):
+		if torch.isnan(loss):
+			raise ValueError(f"Loss is NaN value. Consecutive values - dot: {dot},\
+				norm original: {norm_original}, norm reconstructed: {norm_reconstructed},\
+				norm product: {norm_product}, argument: {argument}, acos: {acos},\
+				loss: {loss}, input: {tensor_pred}, output: {target}")
+		return loss
+
+class Loss_SID(nn.Module):
 	""" Computes the Spectral Information Divergence Loss (PyTorch - Training Loss) """
-	EPS = 1e-3
-	output = torch.clamp(tensor_pred, 0, 1)
-	a1 = output * torch.log10((output + EPS) / (tensor_gt + EPS))
-	a2 = tensor_gt * torch.log10((tensor_gt + EPS) / (output + EPS))
+	def __init__(self):
+		super(Loss_SID, self).__init__()
+	
+	def forward(self, tensor_pred, tensor_gt):
+		assert tensor_pred.shape == tensor_gt.shape
+		EPS = 1e-3
+		output = torch.clamp(tensor_pred, 0, 1)
+		a1 = output * torch.log10((output + EPS) / (tensor_gt + EPS))
+		a2 = tensor_gt * torch.log10((tensor_gt + EPS) / (output + EPS))
 
-	a1_sum = a1.sum(dim=3).sum(dim=2)
-	a2_sum = a2.sum(dim=3).sum(dim=2)
+		a1_sum = a1.sum(dim=3).sum(dim=2)
+		a2_sum = a2.sum(dim=3).sum(dim=2)
 
-	sid = torch.mean(torch.abs(a1_sum + a2_sum))
-	if torch.isnan(sid):
-		raise ValueError(f"Loss is NaN value. output: {output},\
-			a1: {a1}, a1_sum: {a1_sum},\
-			a2: {a2}, a2_sum: {a2_sum},\
-			sid: {sid}, input: {tensor_pred}, output: {tensor_gt}")
-	return sid
-
-def weighted_loss(tensor_pred, tensor_gt):
-	""" Computes the weighted loss (PyTorch - Training Loss) """
-	weights = torch.zeros_like(tensor_pred)
-	return torch.sum(weights * torch.abs(tensor_pred - tensor_gt))
+		sid = torch.mean(torch.abs(a1_sum + a2_sum))
+		if torch.isnan(sid):
+			raise ValueError(f"Loss is NaN value. output: {output},\
+				a1: {a1}, a1_sum: {a1_sum},\
+				a2: {a2}, a2_sum: {a2_sum},\
+				sid: {sid}, input: {tensor_pred}, output: {tensor_gt}")
+		return sid
 
 def mse(img_pred, img_gt):
 	""" Calculate the mean square error (NumPy - used in test_psnr())"""
@@ -101,7 +112,7 @@ def test_msam(img_pred, img_gt, max_value=1):
 
 def test_sid(img_pred, img_gt, max_value=1):
 	""" mean spectral information divergence """
-	img_pred = np.clip(img_pred, 0, 1)
+	# img_pred = np.clip(img_pred, 0, 1)
 	img_pred_flat = img_pred.reshape(-1, img_pred.shape[2])
 	img_gt_flat = img_gt.reshape(-1, img_gt.shape[2])
 	assert len(img_pred_flat) == len(img_gt_flat)
