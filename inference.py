3a2a32e36229a39c9650589d5f931bf7c7b9ef9b
Shahzaib Waseem <msw10@sfu.ca>
2024-05-11 14:00:21 -0700

    added transfer learning logic and removed patched_inference

--- inference.py
+++ inference.py
@@ -18,7 +18,7 @@ from config import GT_RGBN_DIR_NAME, GT_REMOVED_IR_CUTOFF_DIR_NAME, GT_AUXILIARY
 	MOBILE_DATASET_DIR_NAME, MOBILE_RECONSTRUCTED_HS_DIR_NAME, GT_REMOVED_IR_CUTOFF_RECONSTRUCTED_DIR_NAME, PATCHED_HS_DIR_NAME, TRAIN_VAL_TEST_SPLIT_DIR_NAME,\
 	PATCHED_INFERENCE, CLASSIFICATION_PATCH_SIZE, STRIDE, DATA_PREP_PATH, GT_DATASET_CROPS_FILENAME, MOBILE_DATASET_CROPS_FILENAME,\
 	TEST_DATASETS, TEST_ROOT_DATASET_DIR, MODEL_PATH, APPLICATION_NAME, BANDS, EPS,\
-	device, var_name, use_mobile_dataset, model_run_title, checkpoint_file
+	device, var_name, use_mobile_dataset, transfer_learning, model_run_title, checkpoint_file
 
 def calculate_metrics(img_pred, img_gt):
 	mrae = test_mrae(img_pred, img_gt)
@@ -30,7 +30,7 @@ def calculate_metrics(img_pred, img_gt):
 	ssim_db = test_ssim_db(img_pred, img_gt, max_value=1)
 	return mrae, rrmse, msam, sid, psnr, ssim, ssim_db
 
-def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_inference=False):
+def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_inference=False, transfer_learning=transfer_learning):
 	# input_transform, label_transform = get_required_transforms(task="reconstruction")
 	logger = initialize_logger(filename="test.log")
 	log_string = "[%15s] Time: %0.9f, MRAE: %0.9f, RRMSE: %0.9f, SAM: %0.9f, SID: %0.9f, PSNR: %0.9f, SSIM: %0.9f, SSIM (dB): %0.9f"
@@ -79,7 +79,6 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 
 		print("Dataset: %s\nTest Directory: %s\nModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
 		logger.info("Dataset: %s\tTest Directory: %s\tModel: %s\n" % (APPLICATION_NAME, test_dataset, checkpoint_filename))
-		p_mrae, p_rrmse, p_msam, p_sid, p_psnr, p_ssim, num_patches = 0, 0, 0, 0, 0, 0, 0
 
 		with open(os.path.join(TEST_DATASET_DIR, "%s_204ch" % test_dataset, TRAIN_VAL_TEST_SPLIT_DIR_NAME, "test.txt"), "r") as test_file:
 			hypercube_list = [filename.replace("\n", ".mat") for filename in test_file]
@@ -87,9 +86,15 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 		for filename in hypercube_list:
 			start_time = time.time()
 			mat_number = filename.split("_")[0].split(".")[0]
+			crop_record = crops_df[crops_df["image"].isin(["{}_RGB.png".format(mat_number)])]
+			xmin = int(crop_record["xmin"].iloc[0])
+			ymin = int(crop_record["ymin"].iloc[0])
+			xmax = int(crop_record["xmax"].iloc[0])
+			ymax = int(crop_record["ymax"].iloc[0])
 
 			hypercube = load_mat(os.path.join(directory, GT_HYPERCUBES_DIR_NAME, filename))
 			hypercube = hypercube[:, :, BANDS]
+			hypercube = hypercube[ymin:ymax, xmin:xmax, :] if transfer_learning else hypercube
 			hypercube = (hypercube - hypercube.min()) / (hypercube.max() - hypercube.min())
 			min_hc, max_hc = min(min_hc, hypercube.min()), max(max_hc, hypercube.max())
 			hypercube = hypercube + EPS
@@ -103,8 +108,9 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 			nir_image = (nir_image - nir_image.min()) / (nir_image.max() - nir_image.min())
 			nir_image = np.expand_dims(np.asarray(nir_image), axis=-1)
 
-			image = np.dstack((rgb_image, nir_image))
 			# image = rgb_image
+			image = np.dstack((rgb_image, nir_image))
+			image = image[ymin:ymax, xmin:xmax, :] if transfer_learning else image
 			image = np.transpose(image, [2, 0, 1])
 			image = np.expand_dims(image, axis=0)
 			image_tensor = torch.Tensor(image).float().to(device)
@@ -150,48 +156,6 @@ def inference(model, checkpoint_filename, mobile_reconstruction=False, patched_i
 				print("[%15s] Time: %0.9f" % (filename, end_time))
 				logger.info("[%15s] Time: %0.9f" % (filename, end_time))
 
-			if patched_inference:
-				crop_record = crops_df[crops_df["image"].isin(["{}_RGB.png".format(mat_number)])]
-				hypercube_combined = {}
-				xmin = int(crop_record["xmin"].iloc[0])
-				ymin = int(crop_record["ymin"].iloc[0])
-				xmax = int(crop_record["xmax"].iloc[0])
-				ymax = int(crop_record["ymax"].iloc[0])
-				# hypercube_joined = hypercube.copy()
-
-				for patch_i in range(xmin, xmax, CLASSIFICATION_PATCH_SIZE):
-					if patch_i+CLASSIFICATION_PATCH_SIZE > xmax: continue
-					for patch_j in range(ymin, ymax, CLASSIFICATION_PATCH_SIZE):
-						if patch_j+CLASSIFICATION_PATCH_SIZE > ymax: continue
-						imageCrop = image[:, :, patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE]
-						hypercubeCrop = hypercube[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :]
-						# hypercubeCrop = np.transpose(hypercubeCrop, [1, 2, 0])
-						if (imageCrop.shape[2:4] != (CLASSIFICATION_PATCH_SIZE, CLASSIFICATION_PATCH_SIZE)):
-							# hypercube_joined[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :] = 0
-							continue
-						image_tensor = torch.Tensor(imageCrop).float().to(device)
-						with torch.no_grad():
-							hypercube_pred = model(image_tensor)
-						# hypercube_pred = hypercube_pred.squeeze(0).cpu().detach().numpy()
-						hypercube_pred = np.transpose(hypercube_pred.squeeze(0).cpu().detach().numpy(), [1, 2, 0]) + EPS
-						if not mobile_reconstruction:
-							mrae, rrmse, msam, sid, psnr, ssim, ssim_db = calculate_metrics(hypercube_pred + EPS, hypercubeCrop)
-							p_mrae += mrae
-							p_rrmse += rrmse
-							p_msam += msam
-							p_sid += sid
-							p_psnr += psnr
-							p_ssim += ssim
-							p_ssim_db += ssim_db
-							num_patches += 1
-
-						# hypercube_joined[patch_i:patch_i+CLASSIFICATION_PATCH_SIZE, patch_j:patch_j+CLASSIFICATION_PATCH_SIZE, :] = hypercube_pred
-						# patches.append((patch_i, patch_j))
-						# print(patch_i, patch_j, hypercube_pred.shape, hypercubeCrop.shape)
-
-						hypercube_combined[u"(%d, %d)"% (patch_i, patch_j)] = hypercube_pred
-				print(log_string % (filename+"_patch", end_time, p_mrae/num_patches, p_rrmse/num_patches, p_msam/num_patches, p_sid/num_patches, p_psnr/num_patches, p_ssim/num_patches, p_ssim_db/num_patches)) if not mobile_reconstruction else None
-				save_mat_patched(os.path.join(OUT_PATH, PATCHED_HS_DIR_NAME, filename), hypercube_combined)
 		print(log_string_avg % ("Average %s" % test_dataset, losses_mrae.avg, losses_mrae.stddev, losses_rmse.avg, losses_rmse.stddev,
 						  losses_sam.avg, losses_sam.stddev, losses_sid.avg, losses_sid.stddev,
 						  losses_psnr.avg, losses_psnr.stddev, losses_ssim.avg, losses_ssim.stddev, losses_ssim_db.avg, losses_ssim_db.stddev))
@@ -220,7 +184,7 @@ def main():
 	model = model.to(device)
 	model.eval()
 	summary(model=model, input_data=(4, 512, 512))
-	inference(model, checkpoint_filename, mobile_reconstruction=use_mobile_dataset, patched_inference=PATCHED_INFERENCE)
+	inference(model, checkpoint_filename, mobile_reconstruction=use_mobile_dataset, patched_inference=PATCHED_INFERENCE, transfer_learning=transfer_learning)
 
 if __name__ == "__main__":
 	main()
